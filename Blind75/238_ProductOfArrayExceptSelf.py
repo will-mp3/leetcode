266e72e9601e64bfe19cf6d6164fe7c6e8bada92
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-16 12:41:58 -0500

    added prompt and explanation to 238

--- Blind75/238_ProductOfArrayExceptSelf.py
+++ Blind75/238_ProductOfArrayExceptSelf.py
@@ -1,3 +1,11 @@
+"""
+Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+You must write an algorithm that runs in O(n) time and without using the division operation.
+"""
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         result = [1] * (len(nums))
@@ -12,3 +20,14 @@ class Solution:
             post *= nums[i]
     
         return result
+
+"""
+the first thing to consider when solving this problem are the specific parameters given at the bottom:
+we must solve this problem in O(n) time and cannot use the division operator.
+to start we initialize our output array result.
+there are two loops in this solution:
+one going through and storing the prefix value in result[i] and then recalculating the prefix value with nums[i]
+and another going through and multiplying the stored prefix from before with the current postfix value and then recaluating that like above.
+what this does is multiplie all the before values (prefix) and after values (postfix) of i together and stores them in the result[i] slot.
+this solution runs in O(n) linear time.
+"""
