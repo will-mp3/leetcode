6f2bd08aac975787c8efdaced47f9ed5039bd89c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-15 20:27:17 -0500

    added code for 152

---
+++ 152_MaximumProductSubarray.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = max(nums)
+        curMin, curMax = 1, 1
+
+        for n in nums:
+            if n == 0:
+                curMin, curMax = 1, 1
+                continue
+            temp = curMax * n
+            curMax = max(n * curMax, n * curMin, n)
+            curMin = min(temp, n * curMin, n) # bugfix to avoid a change in curMax affecting the min
+            res = max(res, curMax)
+        return res
