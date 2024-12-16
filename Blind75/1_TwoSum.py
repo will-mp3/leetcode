85c9be2fbab9a208d2eeeea71b99805065f23352
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-16 10:33:19 -0500

    added prompt and explanation to 1

--- Blind75/1_TwoSum.py
+++ Blind75/1_TwoSum.py
@@ -1,3 +1,11 @@
+"""
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+"""
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hMap = {} # val : index
@@ -8,3 +16,10 @@ class Solution:
                 return [hMap[diff], i]
             hMap[n] = i
         return
+
+"""
+Create an empty dictionary (hash table). 
+find the difference between the target value and the current number in the array. 
+Using the array given and the enumerate function iterate through the values and check if the diff variable is in the hash table. 
+If it is return, otherwise continue iterating, adding the value and index to the dictionary with each execution.
+"""
