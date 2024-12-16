9303907e84a768d068a427661efd4268f9d23785
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-16 10:42:11 -0500

    added prompt and solution to 53

--- Blind75/53_MaximumSubarray.py
+++ Blind75/53_MaximumSubarray.py
@@ -1,3 +1,7 @@
+"""
+Given an integer array nums, find the subarray with the largest sum, and return its sum.
+"""
+
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         maxSub = nums[0]
@@ -8,3 +12,11 @@ class Solution:
             curSum += n
             maxSub = max(maxSub, curSum)
         return maxSub
+
+"""
+begin by tracking the max subarray and the current sum, two variable we will be comparing.
+iterate through the list, adding each variable and evaluating two things:
+is the current sum (before n is added) negative? if so reset it to zero, the previous values add nothing.
+next add n to the current sum and check: is the current sum more than the current max value? do this through the entire list.
+this silution runs in O(n) linear time.
+"""
