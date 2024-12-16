e24fbe119ed5efec39765ef14e3c53e4da896a8c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-16 11:14:29 -0500

    added prompt and explanation to 121

--- Blind75/121_BestTimeToBuyAndSellStock.py
+++ Blind75/121_BestTimeToBuyAndSellStock.py
@@ -1,3 +1,11 @@
+"""
+You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+"""
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         l, r = 0, 1 # left and right pointers
@@ -12,3 +20,12 @@ class Solution:
             r += 1
         
         return maxP
+
+"""
+to solve this problem we wanted to make use of two pointers and a variable tracking our max profit.
+iterate through the list, first checking if the left pointer is less than the right pointer (profitable).
+if so: calculate the profit from the current buy and sell points (left and right) and compare it to the current max profit.
+if left is greater than right, reset the pointers to equal eachother.
+finish the loop incrementing the right pointer by 1.
+this solution runs in O(n) linear time.
+"""
