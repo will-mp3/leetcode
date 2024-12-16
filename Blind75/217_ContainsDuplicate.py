6cf8e3398847b19a00ad72626b222190a7f5053a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-16 12:29:50 -0500

    added prompt and explanation for 217

--- Blind75/217_ContainsDuplicate.py
+++ Blind75/217_ContainsDuplicate.py
@@ -1,3 +1,7 @@
+"""
+Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+"""
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         hSet = set()
@@ -7,3 +11,13 @@ class Solution:
                 return True
             hSet.add(n)
         return False
+
+"""
+to solve this problem we begin by initializing a hashset.
+using our hashset, we iterate through the list and check if n is in our hashset.
+if true, return True.
+if false, add n to our hashset.
+continue looping until a duplicate is found or the loop completes and returns False.
+this solution uses extra memory, the hashset, in return for a better runtime.
+this solution runs is O(n) linear time and has a space complexity of O(n) due to the hashset.
+"""
