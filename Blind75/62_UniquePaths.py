7b307cb554fc5abe6a734c3345df9722b3bd5111
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-21 17:38:51 -0500

    added solution for 62

--- Blind75/62_UniquePaths.py
+++ Blind75/62_UniquePaths.py
@@ -8,8 +8,25 @@ Given the two integers m and n, return the number of possible unique paths that 
 The test cases are generated so that the answer will be less than or equal to 2 * 109.
 """
 
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        row = [1] * n
 
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        
+        return row[0]
 
 """
-
+to solve this problem we will use a dynamic programming approach.
+we know that the final space, the goal, is in the bottom right corner so like many dp approaches we will start there.
+this algorithm works by working backward from that goal position, tallying up the number of possible moves in each square.
+we know that you can only move right or down, so to calculate the total amount of moves, 
+we add the value stored in fields to the right and below our current index.
+the bottom row and rightmost column will be 1 by default since we cannot move down and right respectively.
+as we go through the array the numbers add up and ultimately our starting point [0][0] will have the maximum number of combinations.
+this solution runs in O(n * m).
 """
