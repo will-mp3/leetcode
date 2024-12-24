1aa753e9349c82357d5d7d895a79582db3e876d0
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-24 13:35:05 -0500

    added code for 57

--- Blind75/57_InsertInterval.py
+++ Blind75/57_InsertInterval.py
@@ -11,7 +11,22 @@ Return intervals after the insertion.
 Note that you don't need to modify intervals in-place. You can make a new array and return it.
 """
 
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        res = []
 
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]: # not overlapping
+                res.append(newInterval)
+                return res + intervals[i:]
+            elif newInterval[0] > intervals[i][1]: # not overlapping
+                res.append(intervals[i])
+            else:
+                newInterval = [min(newInterval[0], intervals[i][0]), max(newInterval[1], intervals[i][1])]
+
+        res.append(newInterval)
+
+        return res
 
 """
 
