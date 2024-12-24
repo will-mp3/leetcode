6ed1189542fae2785d114450684d205408fde22b
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-24 12:18:30 -0500

    added file for 323

---
+++ Blind75/323_NumberOfConnectedComponentsInAnUndirectedGraph.py
@@ -0,0 +1,7 @@
+"""
+You have a graph of n nodes. You are given an integer n and an array edges where edges[i] = [ai, bi] 
+indicates that there is an edge between ai and bi in the graph.
+
+Return the number of connected components in the graph.
+"""
+
