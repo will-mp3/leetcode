9320eece67bfa40aec2af12e0615690bfe9f95e0
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-26 17:47:58 -0500

    added solution for 141

--- Blind75/141_LinkedListCycle.py
+++ Blind75/141_LinkedListCycle.py
@@ -8,8 +8,33 @@ Note that pos is not passed as a parameter.
 Return true if there is a cycle in the linked list. Otherwise, return false.
 """
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        slow, fast = head, head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
 
 """
-
+to solve this problem we can easily use a hast set to iterate through the list and check if a node has already been visited.
+however, if we want to optimize this and acheive linear time AND constant space complexity we can use floyd's tortoise and hare algorithm.
+the way this works is we have a slow pointer and a fast pointer.
+the slow pointer increments by 1 every cycle and the fast pointer increments by 2 every cycle.
+if at any point the fast pointer reaches the end of the list we know there is no loop.
+to check if there is a loop, we continue to iterate and if slow and fast ever equal eachother then we know there is a loop.
+this is because while the fast pointer will start in front of the slow pointer after iteration 1,
+if they are moving cyclically the fast pointer will continue to close the gap by one space each iteration, eventually reaching the slow pointer.
+this solution runs in O(n) linear time and has a space complexity of O(1).
 """
