d2a599ad2b07ddbc14daf93b1e4a3754ff96b88a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-29 19:31:02 -0500

    updated solution

--- MEDIUM/3_LongestSubstringWithoutRepeatingCharacters.py
+++ MEDIUM/3_LongestSubstringWithoutRepeatingCharacters.py
@@ -9,7 +9,7 @@ class Solution:
         res = 0
 
         for r in range(len(s)):
-            while s[r] in charSet:
+            while s[r] in charSet: # remove left pointer values into s[r] is out of the charSet
                 charSet.remove(s[l])
                 l += 1
             charSet.add(s[r])
@@ -21,7 +21,8 @@ class Solution:
 this solution makes use of a sliding window.
 the way this works is we initialize a left and right pointer, the left starts at 0 and the right continuously increments.
 as the right pointer goes through the list, if the character is unique we add it to our charSet and calculate a new result.
-once a duplicate character is found in our set, we remove the character at the left pointer and increment it, shrinking the window.
+once a duplicate character is found in our set, we remove the character at the left pointer until the duplicate character (at r) is gone. 
+we continuously update the left pointer to shrink the window as this is happening.
 as these two results ebb and flow we continue to update our result with the max between the current result and size of current window.
 once the loop completes we return our result.
 this solution runs in O(n) linear time.
