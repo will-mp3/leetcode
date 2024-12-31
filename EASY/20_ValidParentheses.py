bd7114ac18ab08f10a4e4a5ab4a780937a6b2ef9
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-31 11:54:14 -0500

    added explanation for 20

--- EASY/20_ValidParentheses.py
+++ EASY/20_ValidParentheses.py
@@ -25,5 +25,11 @@ class Solution:
         return True if not stack else False
 
 """
-
+to solve this problem we make use of a stack data structure (just like the problem from CS241...).
+the reason we can utilize a stack here is because of the paired nature of parentheses.
+with our stack we are able to continually take in open parentheses and then pop these as matching closed parentheses come in.
+for example we could receive "( { [ (" as our first four values, as the next four come we can check if they match and pop the top value.
+the next four would be ") ] } )" and instead of adding them we pop the top value: ( and so on.
+if our stack is empty by the end then we know we have a valid string of parentheses, else its invalid.
+this solution runs in O(n) linear time.
 """
