8e666acc9104e6ffcbed27640ba5b65dc5892c9f
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-31 11:46:30 -0500

    added code for 20

--- EASY/20_ValidParentheses.py
+++ EASY/20_ValidParentheses.py
@@ -8,7 +8,21 @@ Open brackets must be closed in the correct order.
 Every close bracket has a corresponding open bracket of the same type.
 """
 
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+        closeToOpen = { ")" : "(", "]" : "[" , "}" : "{" }
 
+        for c in s:
+            if c in closeToOpen: # checks if c is a closing parenthesis
+                if stack and stack[-1] == closeToOpen[c]: # check that stack is not empty and value at the top of stack is a matching open parenthesis
+                    stack.pop()
+                else:
+                    return False
+            else: # we got an open parenthesis
+                stack.append(c) # can add as many open parentheses as we want consecutively
+
+        return True if not stack else False
 
 """
 
