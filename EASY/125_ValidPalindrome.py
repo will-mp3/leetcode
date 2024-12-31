77af60f9a2e6535e141d2a49ee0d343a1ef04f30
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2024-12-31 12:50:28 -0500

    added code and explanation for 125

--- EASY/125_ValidPalindrome.py
+++ EASY/125_ValidPalindrome.py
@@ -7,8 +7,18 @@ Alphanumeric characters include letters and numbers.
 Given a string s, return true if it is a palindrome, or false otherwise.
 """
 
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        newStr = ""
 
+        for c in s:
+            if c.isalnum(): # check if character is alphanumerical
+                newStr += c.lower()
+
+        return newStr == newStr[::-1] # reversed string
 
 """
-
+this solution is very simple.
+our approach is to reformat the given string, removing all non alphanumerial values and making every character lowercase.
+once done we simply check if our new string is equal to its reversed self.
 """
