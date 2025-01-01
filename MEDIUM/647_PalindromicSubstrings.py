27a034a1a58c7940eb85f4ea63a1a32d2758e435
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-01 14:10:25 -0500

    added code for 647

--- MEDIUM/647_PalindromicSubstrings.py
+++ MEDIUM/647_PalindromicSubstrings.py
@@ -6,7 +6,26 @@ A string is a palindrome when it reads the same backward as forward.
 A substring is a contiguous sequence of characters within the string.
 """
 
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        res = 0
 
+        for i in range(len(s)):
+            # odd length palindromes
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]: # check if pointers are in bounds and contain a palindrome
+                res += 1
+                l -= 1
+                r += 1
+            
+            # check even length palindromes
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]: # check if pointers are in bounds and contain a palindrome
+                res += 1
+                l -= 1
+                r += 1
+
+        return res
 
 """
 
