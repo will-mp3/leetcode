588ad94ecef4bfae64421418964c93bf0e405cc2
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-01 15:39:27 -0500

    added code for 271

--- MEDIUM/271_EncodeAndDecodeStrings.py
+++ MEDIUM/271_EncodeAndDecodeStrings.py
@@ -26,7 +26,33 @@ Implement the encode and decode methods.
 You are not allowed to solve the problem using any serialize methods (such as eval).
 """
 
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        res = ""
 
+        for s in strs:
+            # encode our string with the number of characters, a pound sign, and its contents
+            res += str(len(s)) + "#" + s
+
+        return res
+        
+
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        res, i = [], 0
+         
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j]) # once we hit our delimiter get our encoded length value
+            res.append(s[j + 1 : j + 1 + length])
+            i = j + 1 + length
+
+        return res
 
 """
 
