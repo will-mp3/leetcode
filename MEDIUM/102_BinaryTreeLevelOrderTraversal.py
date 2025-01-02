177991720e1a90054feb69abd4c0f57967f4601e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-02 13:37:28 -0500

    added explanation for 102

--- MEDIUM/102_BinaryTreeLevelOrderTraversal.py
+++ MEDIUM/102_BinaryTreeLevelOrderTraversal.py
@@ -33,5 +33,16 @@ class Solution:
         return res
 
 """
-
-"""
+to solve this problem, we utilize breadth first search.
+to implement this algorithm, we utilize a queue.
+we start by adding the root to our queue, this initializes it and allows us to start our looping algorithm.
+while the queue is not empty, taken the current length and create an array level to hold the nodes at this current level.
+with our queue length, iterate through that many times, each time popping the left value and adding it to our level array.
+we all add our left and right children to our queue, maintaining the loop.
+lastly we add the level array to our result array and continue the loop.
+each iteration we update the length and reset the level array.
+what this is doing is taking each node for each level and storing them as a list in an array.
+once our queue is empty we know we have searched every node and are done.
+by the end our result array will hold all of our node lists.
+this solution runs in O(n) linear time.
+""" 
