5797f449e867454e72dd880c623cc6bf357280f6
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-02 13:07:38 -0500

    added expanation for 226

--- EASY/226_InvertBinaryTree.py
+++ EASY/226_InvertBinaryTree.py
@@ -23,5 +23,10 @@ class Solution:
         return root
 
 """
-
+to solve this problem we use a recursive depth first search method.
+we break the problem into pieces, essentially dealing with only three nodes at a time, the root and its children.
+we go through, starting at the top, and swap the left and right children of the root, 
+then call the same method on the same left and right children, ending once the root is Null (end of tree)
+once we're finished we return the root.
+this solution runs in O(n) linear time.
 """
