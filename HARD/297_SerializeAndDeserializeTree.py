a45925872d4a374686a9aabef39106cef4058e0e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-02 17:23:21 -0500

    added code for 297

--- HARD/297_SerializeAndDeserializeTree.py
+++ HARD/297_SerializeAndDeserializeTree.py
@@ -11,7 +11,47 @@ Clarification: The input/output format is the same as how LeetCode serializes a 
 You do not necessarily need to follow this format, so please be creative and come up with different approaches yourself.
 """
 
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
+class Codec:
+
+    def serialize(self, root):
+        res = [] # will join with comma delimiter later
+
+        def dfs(node):
+            if not node:
+                res.append("N")
+                return
+            res.append(str(node.val))
+            # pre-order traversal
+            dfs(node.left)
+            dfs(node.right)
+
+        dfs(root)
+        return ",".join(res)
+
+    def deserialize(self, data):
+        vals = data.split(",")
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == "N":
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+
+            node.left = dfs()
+            node.right = dfs()
+            
+            return node
+        
+        return dfs()
 
 """
 
