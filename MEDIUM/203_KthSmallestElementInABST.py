0e57a1d14011603f26dc442b27b9f75263412734
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-04 18:23:36 -0500

    added explanation for 203

--- MEDIUM/203_KthSmallestElementInABST.py
+++ MEDIUM/203_KthSmallestElementInABST.py
@@ -9,7 +9,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         n = 0
@@ -29,5 +29,15 @@ class Solution:
             cur = cur.right
 
 """
-
+this questions asks us to find the kth smallest integer of a binary search tree.
+luckily, there is a traversal method that puts all our values in order, namely in-order traversal (obviously).
+this is an easy thing to implement using recursion, but for the sake of practice we will do the iterative solution instead.
+to start, we initiliaze our count variable n (which we will compare to k), our stack, and set our current node to the root node.
+while our tree still has nodes or our stack still have values we loop.
+each iteration we hit another loop that runs until cur is null, within we add cur to our stack and update cur to its left child.
+once this loop breaks we know we've hit the bottom of our leftmost traversal.
+we pop the stack to update cur, add 1 to n, and check if n is equal to k.
+if n and k are equal, we can return the value of cur, otherwise we set cur to its right value and repeat this logic.
+this loop will completely search the tree using in order traversal.
+this solution runs in O(n) linear time.
 """
