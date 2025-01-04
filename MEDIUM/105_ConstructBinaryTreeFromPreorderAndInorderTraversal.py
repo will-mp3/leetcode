5c009cb8d594aeda17965fcfb6c669ae65aabe57
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-04 16:39:38 -0500

    added explanation for 105

--- MEDIUM/105_ConstructBinaryTreeFromPreorderAndInorderTraversal.py
+++ MEDIUM/105_ConstructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -10,7 +10,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
     def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
         if not preorder or not inorder:
@@ -25,5 +25,17 @@ class Solution:
         return root
 
 """
-
+for this problem we are tasked with recreating a tree using its inorder and preorder traversals.
+we tackle this problem recursively using just one pointer and some array manipulation.
+we will use preorder = [3,9,20,15,7], inorder = [9,3,15,20,7] as our example inputs.
+to solve this problem we start by creating the root of the tree, that will always be the first value in a preorder array.
+next we get our mid pointer, we use this to divide the arrays into left and right subarrays, symbolizing left and right sides of the tree.
+the node creation happens when we make our root node, so once we have that and mid we call the buildTree function again.
+the mid pointer is used here to divide our arrays into smaller subarrays.
+here our mid value is 1. 
+using that we call buildTree on the left, passing just [9] as our preorder (index 1 to mid + 1) and [9] again for our inorder (index 0 to mid).
+we do the same thing on the right, this time passing [20, 15, 7] as preorder (mid + 1 onward) and [15, 20, 7] as our inorder (mid + 1 onward).
+you notice that our values for inorder and preorder are the same, just in a different order.
+that is because they contain all of the values in that side of the remaining tree.
+our buildTree function uses these values until the tree is empty to construct the tree and then returns root back to the top. 
 """
