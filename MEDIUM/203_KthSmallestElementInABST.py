a098e64d67ba26544fc7d7703ab7ee766f931b9d
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-04 17:51:28 -0500

    added code for 203

--- MEDIUM/203_KthSmallestElementInABST.py
+++ MEDIUM/203_KthSmallestElementInABST.py
@@ -3,7 +3,30 @@ Given the root of a binary search tree, and an integer k,
 return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
 """
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        n = 0
+        stack = []
+        cur = root
 
+        while cur or stack: # while cur is not null and stack in nonempty
+            while cur: # go left until null
+                stack.append(cur)
+                cur = cur.left
+
+            cur = stack.pop() # process value
+            n += 1
+            if n == k:
+                return cur.val
+
+            cur = cur.right
 
 """
 
