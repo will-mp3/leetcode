e025c2c4b8b5c59a2652c2e5dac156d6f0806c34
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-04 16:18:11 -0500

    added code for 105

--- MEDIUM/105_ConstructBinaryTreeFromPreorderAndInorderTraversal.py
+++ MEDIUM/105_ConstructBinaryTreeFromPreorderAndInorderTraversal.py
@@ -4,7 +4,25 @@ where preorder is the preorder traversal of a binary tree and inorder is the ino
 construct and return the binary tree.
 """
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+        
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
 
+        root = TreeNode(preorder[0]) # first value in preorder is always the root
+        mid = inorder.index(preorder[0]) # sets out middle position, dividing nodes on the left and right of the root
+        # create sublists
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid]) # every value from index 1 to mid for preoder, every value from the beginning to mid for inorder (draw picture if lost)
+        root.right = self.buildTree(preorder[mid + 1:], inorder[mid + 1:]) # every value from mid + 1 onward for preorder, every value from mid + 1 onward for inorder
+
+        return root
 
 """
 
