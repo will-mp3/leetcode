e892e9d2ed667dd84523e3c5bfa7bde04605ccd7
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-05 13:17:47 -0500

    added code for 208

--- MEDIUM/208_ImplementTrie(PrefixTree).py
+++ MEDIUM/208_ImplementTrie(PrefixTree).py
@@ -10,7 +10,48 @@ boolean search(String word) Returns true if the string word is in the trie (i.e.
 boolean startsWith(String prefix) Returns true if there is a previously inserted string word that has the prefix prefix, and false otherwise.
 """
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.endOfWord = False
 
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children: # check if character is in our hashmap
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        
+        # whether we already had the nodes or had to create them, cur is now the last letter in the word
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        
+        # this loop continues for every character until we find one that dosent exist or we reach then end of the word
+        return cur.endOfWord # we can only return true if this fina character is the end of a word
+
+    def startsWith(self, prefix: str) -> bool:
+        # this method is exactly the same as search, except it dosent matter if the final character is the end of the word
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        
+        return True
 
 """
 
