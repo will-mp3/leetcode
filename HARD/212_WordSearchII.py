c64b10abe21817392f0ebbe9316c8c8f9416e687
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-05 15:50:15 -0500

    added explanation for 212

--- HARD/212_WordSearchII.py
+++ HARD/212_WordSearchII.py
@@ -52,5 +52,19 @@ class Solution:
         return list(res)
 
 """
-
+to solve this problem effeciently we will make use of a trie (prefix tree) data structure.
+the basic logic of the tree is each node will have a dictionary of children, this will allow us to quickly acces which children it may have.
+the reason this is necesary is each node can have up to 26 children, lowercase a-z.
+when we create a node we also give it a variable isWord to track if its the end of a word, useful later.
+to insert a word we check to see if the current character is a child of our current node, if not we create a new node for that character.
+once the entire word has been added, using existing nodes or new nodes, we set its last character nodes endOfWord variable to True.
+we start by adding all of the words in our word list to this trie structure for future access.
+using this, we create a dfs function to go through the entire matrix of characters.
+we first check if the character is valid, its inbounds and the character is in the children set for our node.
+if this is true we add it to our visit set, update the node to this child, add the character to our word string, and check isWord.
+if this is the end of the word, we add the word string to result. 
+then call the dfs function on the every direction from our current node.
+once this is finished executing we remove the cell from our visit set.
+outside of the dfs function we create a nested loop going through every cell in our matric, calling dfs on every position.
+when this is finished running we return our list of words.
 """
