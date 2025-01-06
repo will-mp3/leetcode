c2f781e8bbe6dc6d13364169132dd99bf4b4942e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-06 18:27:01 -0500

    added code for 347

--- MEDIUM/347_TopKFrequentElements.py
+++ MEDIUM/347_TopKFrequentElements.py
@@ -3,7 +3,23 @@ Given an integer array nums and an integer k, return the k most frequent element
 You may return the answer in any order.
 """
 
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        count = {}
+        freq = [[] for i in range(len(nums) + 1)]
 
+        for n in nums:
+            count[n] = 1 + count.get(n, 0) # increments count of number in dictionary, if it dosent exist get adds 0
+
+        for n, c in count.items():
+            freq[c].append(n) # this value n occurs c number of times
+
+        res = []
+        for i in range(len(freq) - 1, 0, -1):
+            for n in freq[i]:
+                res.append(n)
+                if len(res) == k:
+                    return res
 
 """
 
