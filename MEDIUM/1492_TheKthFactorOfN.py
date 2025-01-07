2376b399b9471292d1253a2976960bee92d0885c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-07 17:00:24 -0500

    added code for 1492

--- MEDIUM/1492_TheKthFactorOfN.py
+++ MEDIUM/1492_TheKthFactorOfN.py
@@ -5,7 +5,20 @@ A factor of an integer n is defined as an integer i where n % i == 0.
 Consider a list of all factors of n sorted in ascending order, return the kth factor in this list or return -1 if n has less than k factors.
 """
 
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        factors = []
+        x = 1
 
+        while x <= n:
+            if n % x == 0:
+                factors.append(x)
+            x += 1
+        
+        if len(factors) < k:
+            return -1
+        
+        return factors[k - 1]
 
 """
 
