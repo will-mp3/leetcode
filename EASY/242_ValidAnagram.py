37a50006f3549574086ddbd998fd33c57d290ae3
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-06 21:24:17 -0500

    added explanation for 242

--- EASY/242_ValidAnagram.py
+++ EASY/242_ValidAnagram.py
@@ -5,10 +5,10 @@ Given two strings s and t, return true if t is an anagram of s, and false otherw
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # solution 2, O(1) space complexity assuming sorting algorithms are considered negligible
-        return sorted(s) == sorted(t)
+        # return sorted(s) == sorted(t)
 
         # solution 3, kinda cheating
-        return Counter(s) == Counter(t)
+        # return Counter(s) == Counter(t)
 
         # solution 1, what you would typically submit
         if len(s) != len(t):
@@ -26,5 +26,13 @@ class Solution:
         return True
 
 """
-
+this problem we are tasked with identifying anagrams, or more simply checking if strings contain identical characters.
+above are three solutions, solution 2 sorts the strings alphabetically and compares if they are equal, meh.
+solution 3 using a built in function thats main purpose is comparing characters, wouldnt recommend for an interview.
+solution 1 is what I would recommend submitting, it makes use of two hashmaps to track character counts.
+we start by ensuring the strings are the same length, if they arent return false.
+we create and build two hashmaps by iterating through our strings and updating the counts of each character as we cross them.
+once our hashmaps are built, we iterate once more to ensure the characters and counts are equal.
+if at any point they are not equal return false, if our loop runs through completely return true.
+this solution runs in O(n) linear time.
 """
