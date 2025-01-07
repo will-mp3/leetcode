e9e4f5c674f2c52f64387ef19088f1f114782461
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-06 21:48:39 -0500

    added explanation for 371

--- MEDIUM/371_SumOfTwoIntegers.java
+++ MEDIUM/371_SumOfTwoIntegers.java
@@ -14,5 +14,15 @@
 }
 
  /*
-    
+    for this solution I opted to use java since I prefer its binary arithmetic over python.
+    to solve this problem we make use of binary addition, we do this using the xor and and operators.
+    xor gives us a binary string with all of the 1s where a 0 and 1 have been added, specifically all of the 1s that dont require a carry.
+    a carry happens when 1 and 1 are added in binary, this results in a 0 in that position and a 1 carried to the next position.
+    because our xor is missing these carries, we account for this by taking the and of the strings, this gives us all the 1s that we missed.
+    we must also bit shift this 1 to the left to move the carrys into their correct positions.
+    both of these together simulate addition, and assume that there will be carrys.
+    once we do our xor operator and have no carrys, we know that our addition is complete.
+    to check for this we loop until b, our and operator, is 0.
+    if b is zero it means that a and b have no overlapping 1s, and therefor there are no carrys when we xor.
+    once this loop terminates we return a, the xor of the current a and b binary strings.
   */
