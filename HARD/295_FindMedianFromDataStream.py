d8b9e3e293525e939a30061d3178cfa872ad3254
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-06 20:33:04 -0500

    added code for 295

--- HARD/295_FindMedianFromDataStream.py
+++ HARD/295_FindMedianFromDataStream.py
@@ -12,7 +12,41 @@ double findMedian() returns the median of all elements so far.
 Answers within 10-5 of the actual answer will be accepted.
 """
 
+class MedianFinder:
 
+    def __init__(self):
+        # two heaps, large and small
+        # large is a min heap, small is a max heap
+        # heaps must should be equal size
+        self.small, self.large = [], []
+
+    def addNum(self, num: int) -> None:
+        # note: we multiply our max heap by -1 to get its true value since python only declares min heaps
+        heapq.heappush(self.small, -1 * num) # python only implements min heaps, we multiply by -1 to simulate a max heap
+
+        # make sure every element in small <= every element in large
+        if (self.small and self.large and (-1 * self.small[0]) > self.large[0]): # (-1 * self.small[0]) is the largest value in small, self.large[0] is the smallest number in large
+            val = -1 * heapq.heappop(self.small) # pop largest value from small heap, store it in val
+            heapq.heappush(self.large, val) # push the largest value in small to large
+
+        # size difference handling
+        if len(self.small) > len(self.large) + 1:
+            val = -1 * heapq.heappop(self.small) # pop largest value from small heap, store it in val
+            heapq.heappush(self.large, val) # push the largest value in small to large
+        if len(self.large) > len(self.small) + 1:
+            val = heapq.heappop(self.large) # pop largest value from small heap, store it in val
+            heapq.heappush(self.small, -1 * val) # push the largest value in small to large
+        
+
+    def findMedian(self) -> float:
+        # odd length handling, returns middle value
+        if len(self.small) > len(self.large):
+            return -1 * self.small[0]
+        if len(self.large) > len(self.small):
+            return self.large[0]
+        
+        # even length handling, returns middle two values average
+        return (-1 * self.small[0] + self.large[0]) / 2
 
 """
 
