5bbeb21a380606087ede0dba1f269742433d4942
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-06 21:48:29 -0500

    added code for 371

--- MEDIUM/371_SumOfTwoIntegers.java
+++ MEDIUM/371_SumOfTwoIntegers.java
@@ -2,7 +2,16 @@
     Given two integers a and b, return the sum of the two integers without using the operators + and -.
  */
 
-
+ class Solution {
+    public int getSum(int a, int b) {
+        while (b != 0) {
+            int tmp = (a & b) << 1;
+            a = a ^ b; // xor a and b, this results in our binary string with all ones except the carry values
+            b = tmp; // & a and b, this results in our binary string with just the missing carry values, we shift by 1 to put them in their correct space
+        }
+        return a;
+    }
+}
 
  /*
     
