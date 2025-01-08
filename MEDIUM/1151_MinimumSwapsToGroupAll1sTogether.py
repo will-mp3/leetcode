e309b12b8fd113e933072c14e29b1bbc239cb821
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-08 17:31:28 -0500

    added code for 1151

--- MEDIUM/1151_MinimumSwapsToGroupAll1sTogether.py
+++ MEDIUM/1151_MinimumSwapsToGroupAll1sTogether.py
@@ -3,7 +3,23 @@ Given a binary array data,
 return the minimum number of swaps required to group all 1's present in the array together in any place in the array.
 """
 
+class Solution:
+    def minSwaps(self, data: List[int]) -> int:
+        ones = sum(data)
+        l, r = 0, 0
+        maxOnes = countOnes = 0
 
+        while (r < len(data)):
+            countOnes += data[r]
+            r += 1
+
+            if (r - l) > ones:
+                countOnes -= data[l]
+                l += 1
+
+            maxOnes = max(maxOnes, countOnes)
+
+        return ones - maxOnes
 
 """
 
