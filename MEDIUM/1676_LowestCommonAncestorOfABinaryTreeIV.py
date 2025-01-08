82ecce1d961296f505a50f7e1a489e26a374c03b
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-07 19:59:06 -0500

    added code for 1676

--- MEDIUM/1676_LowestCommonAncestorOfABinaryTreeIV.py
+++ MEDIUM/1676_LowestCommonAncestorOfABinaryTreeIV.py
@@ -8,7 +8,46 @@ Extending the definition of LCA on Wikipedia:
 A descendant of a node x is a node y that is on the path from node x to some leaf node.
 """
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', nodes: 'List[TreeNode]') -> 'TreeNode':
+        # set to check if current node is a target node
+        # once a target node is encountered, we know we dont need to go any lower
+        target = set(nodes)
+
+        # dfs function to traverse the tree and search for target nodes
+        def dfs(root):
+            # if root is a target node, return root node to be saved
+            if root in target:
+                return root
+
+            # if root is null, return null. for end of tree and edge case handling
+            if not root:
+                return None
+            
+            # if node is not a target and is not null, we call dfs on our left and right children
+            # the goal of this if to find the earliest left and right target nodes
+            # once the dfs function for both left and right finish, we will know our LCA
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            # if both left and right contain a node, we know the current root is the LCA, it has two target children
+            if left and right:
+                return root
+            # if only left contains a node, we know that either left is the only target node or all target nodes are beneath it
+            if left and not right:
+                return left
+            # if only right contains a node, we know that either right is the only target node or all target nodes are beneath it 
+            if right and not left:
+                return right
+            
+            return None
 
 """
 
