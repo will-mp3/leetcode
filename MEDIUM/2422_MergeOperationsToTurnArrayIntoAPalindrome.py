b95c938122ff7258b285eabbdb5007388d70ff63
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-11 18:25:38 -0500

    added explanation for 2422

--- MEDIUM/2422_MergeOperationsToTurnArrayIntoAPalindrome.py
+++ MEDIUM/2422_MergeOperationsToTurnArrayIntoAPalindrome.py
@@ -35,5 +35,14 @@ class Solution:
         return res
 
 """
-
+this problem requires us to perform merges in order to convert an array of ints into a palindrome.
+unlike traditional palindrome problems where we would go move out from the center, we will start on the edges.
+using a left and right pointer, intialized at the start and end of our array, we check to see if l and r are equal.
+if they are not equal, we evaluate the sign and merge the smaller value since merging can only increase.
+if left is greater than right, we know we have to merge the right values and increment res.
+we save the current r value in a tmp variable, then decrement r and add the new r value with our tmp variable.
+if left is less than right, we merge left values and increment res.
+we save the current l value in a tmp variable, then increment l and add the new l value with our tmp variable.
+if left and right are equal then we perform both of the above operations without incrementing our result.
+this solution runs in O(n) linear time.
 """
