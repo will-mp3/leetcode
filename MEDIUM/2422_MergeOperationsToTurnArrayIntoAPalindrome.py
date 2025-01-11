488f134e233ee88f2d28953445c2886c31b176ba
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-11 18:07:43 -0500

    added code for 2422

--- MEDIUM/2422_MergeOperationsToTurnArrayIntoAPalindrome.py
+++ MEDIUM/2422_MergeOperationsToTurnArrayIntoAPalindrome.py
@@ -8,7 +8,31 @@ For example, if nums = [1,2,3,1], you can apply one operation to make it [1,5,1]
 Return the minimum number of operations needed to turn the array into a palindrome.
 """
 
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        res = 0
 
+        while l < r:
+            if nums[l] > nums[r]:
+                tmp = nums[r]
+                r -= 1
+                nums[r] += tmp
+                res += 1
+            elif nums[l] < nums[r]:
+                tmp = nums[l]
+                l += 1
+                nums[l] += tmp
+                res += 1
+            else:
+                tmp = nums[l]
+                l += 1
+                nums[l] += tmp
+                tmp = nums[r]
+                r -= 1
+                nums[r] += tmp
+
+        return res
 
 """
 
