faa4bd7c9c0e4a247de99db345ab2d46cd2d7c02
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-12 16:27:08 -0500

    added code for 2330

--- MEDIUM/2330_ValidPalindromeIV.py
+++ MEDIUM/2330_ValidPalindromeIV.py
@@ -5,7 +5,28 @@ In one operation, you can change any character of s to any other character.
 Return true if you can make s a palindrome after performing exactly one or two operations, or return false otherwise.
 """
 
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+        count = 0
 
+        # odd length strings
+        if len(s) % 2 != 0:
+            l, r = (len(s) // 2), (len(s) // 2)
+            while l >= 0 and r < len(s):
+                if s[l] != s[r]:
+                    count += 1
+                l -= 1
+                r += 1
+        # even length strings
+        else:
+            l, r = (len(s) // 2) - 1, (len(s) // 2)
+            while l >= 0 and r < len(s):
+                if s[l] != s[r]:
+                    count += 1
+                l -= 1
+                r += 1
+        
+        return True if count <= 2 else False
 
 """
 
