c734b40cc154f1d41a00405b19f396f698241142
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-12 16:39:02 -0500

    added explanation for 2330

--- MEDIUM/2330_ValidPalindromeIV.py
+++ MEDIUM/2330_ValidPalindromeIV.py
@@ -29,5 +29,14 @@ class Solution:
         return True if count <= 2 else False
 
 """
-
+This problem asks that we analyze a potential palindrome, specifically seeing if its possible to be made with two character conversions.
+this first thing we need to understand is that we can check a palindrome by expanding pointers left and right from the center.
+the next thing that we must understand is that if at any point the values at these pointers are not equal, 1 conversion must be made.
+knowing these, and account for even and odd length strings, we create two pointers and a count variable.
+for our odd length strings, left and right start at the same index in the center.
+as they expand out, and while their index is valid, we check to see if the values at their positions are equal.
+if these values are not equal, we increment our count value.
+we repeat this logic for even length strings, this time setting the left pointer one index left.
+we return true if our count value is less than or equal to 2 and false otherwise.
+this solution runs in O(n) linear time.
 """
