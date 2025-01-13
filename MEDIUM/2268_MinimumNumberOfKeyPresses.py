e6973ca8028a9f918a98ffffaf814caa3122f36a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-13 17:35:25 -0500

    added explanation for 2268

--- MEDIUM/2268_MinimumNumberOfKeyPresses.py
+++ MEDIUM/2268_MinimumNumberOfKeyPresses.py
@@ -30,5 +30,19 @@ class Solution:
         return res
 
 """
-
+this problem relies on understanding what is being asked.
+we know that our keypad has 9 characters, and we can arrange it however we would like.
+the logical decision is to map the first 9 characters to unique keys, then the second 9 characters, and so on for even distribution.
+this is simple enough, but for larger strings, we may need to be more strategic in which keys get mapped where so as to reduce presses.
+for example, we wouldnt want a character that appears 10 times mapped to the second or third layer of our key.
+to account for this, we start by storing the frequency of each character in a hashmap called freq (using the collections import).
+we store this as an array of digits, frequencies, in reverse ascending order (largest to smallest).
+we initialize result and count to zero for tracking and then iterate over frequencies.
+we check if the index is divisible by 9, since each index represents a new unique character.
+if its divisible by 9 we increment count by 1, what this is doing is representing each layer of the keypad.
+the first 9 digits have count equal to 1, the next 9 ocunt equals 2, and so on.
+as we are doing this we are incrementing result by the amount of said digit multiplied by its layer.
+if our digit appears 3 times (frequencies[i] = j = 3) and count is at 1 (first layer) we know that we add 3 presses.
+if our digit appears 3 times and count is 2 then we need 6, hence the need for count and its multiplication.
+once the loop finishes we return res.
 """
