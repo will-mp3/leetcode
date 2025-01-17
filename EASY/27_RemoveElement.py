441c1ee895a710821a3741b67ea1a1189b842fd5
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-17 18:37:59 -0500

    added explanation for 27

--- EASY/27_RemoveElement.py
+++ EASY/27_RemoveElement.py
@@ -19,5 +19,14 @@ class Solution:
         return i
 
 """
-
+for this problem we make use of two pointers, specifically taking advantage of the fact that variables can be moved in our input array.
+because we are only counting the first k elements (the number of elements minus those to be removed), we can simply overwrite them.
+what this would look like is if we had an array [1, 5, 6, 8, 3, 4, 9, 5] and we wanted to remove 5,
+our array would become [1, 6, 8, 3, 4, 9, 5, Null].
+what is happening here is every iteration our value at pointer j does not equal i, we copy the value over and increment pointer i with j.
+if we come across our value, we skip this step, and pointer i stays where it is (at this value).
+the next iteration that dosent match this value then gets overridden.
+simply put, pointer i only increments when pointer j is on a value that is not equal to our solution.
+the purpose of pointer i is to overwrite our target value.
+this solution runs in O(n) linear time.
 """
