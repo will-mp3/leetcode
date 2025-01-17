6c719a4e1047f95facb15c757d65244f0e22800b
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-17 15:11:29 -0500

    added code for 88

--- EASY/88_MergeSortedArray.py
+++ EASY/88_MergeSortedArray.py
@@ -9,7 +9,23 @@ To accommodate this, nums1 has a length of m + n, where the first m elements den
 and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
 """
 
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        p1 = m - 1
+        p2 = n - 1
 
+        for p in range(m + n - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
 
 """
 
