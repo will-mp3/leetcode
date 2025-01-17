c1e8216f9bab55ab7815064192547e94de2d3a10
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-17 14:32:28 -0500

    added explanation for 2519

--- HARD/2519_CountTheNumberOfKBigIndices.py
+++ HARD/2519_CountTheNumberOfKBigIndices.py
@@ -20,5 +20,14 @@ class Solution:
         return ans
 
 """
-
+this problem asks us to find the number of indices in a list where there are k amount of smaller values to the left and right of that index.
+A "k-big index" is an index is an index where there are k numbers <= nums[i] before i and k numbers <= nums[i] after i.
+to solve this, we make use of sorted lists (a python3 specific import).
+we create lists lo and hi, lo tracks the numbers before i and hi tracks the numbers after i.
+we iterate through each number in nums, each time using the bisect left method.
+what this does is inserts our passed in value x to the position in which it wont disrupt the order of the sorted list.
+this checks how many numbers in lo (before i) are less than x and how many numbers in hi (after i) are less than x.
+if there are k or more numbers less than nums[i] before and after i (in lo and high), increment ans.
+each iteration we add the current value x to lo and remove it from hi to simulate shifting.
+this solution runs in O(nlogn): add, remove, and bisect_left = logn ; length of nums = n.
 """
