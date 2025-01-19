ecbad62e6ec81f123cf932f1eafbdabbc3396656
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-19 14:31:47 -0500

    fixed text

--- MEDIUM/71_SimplifyPath.py
+++ MEDIUM/71_SimplifyPath.py
@@ -1,12 +1,14 @@
 """
-You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. Your task is to transform this absolute path into its simplified canonical path.
+You are given an absolute path for a Unix-style file system, which always begins with a slash '/'. 
+Your task is to transform this absolute path into its simplified canonical path.
 
 The rules of a Unix-style file system are as follows:
 
 A single period '.' represents the current directory.
 A double period '..' represents the previous/parent directory.
 Multiple consecutive slashes such as '//' and '///' are treated as a single slash '/'.
-Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. For example, '...' and '....' are valid directory or file names.
+Any sequence of periods that does not match the rules above should be treated as a valid directory or file name. 
+For example, '...' and '....' are valid directory or file names.
 The simplified canonical path should follow these rules:
 
 The path must start with a single slash '/'.
