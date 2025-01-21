e6ef656757931ec5eff30bf25d423817809c389c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-21 13:18:11 -0500

    added code for 392

--- EASY/392_IsSubsequence.py
+++ EASY/392_IsSubsequence.py
@@ -6,7 +6,16 @@ A subsequence of a string is a new string that is formed from the original strin
 (i.e., "ace" is a subsequence of "abcde" while "aec" is not).
 """
 
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l, r = 0, 0 # left pointer for s, right for t
 
+        while l < len(s) and r < len(t):
+            if s[l] == t[r]: # match found
+                l += 1
+            r += 1
+
+        return l == len(s) # if left pointer equals the length of s, we know we have found all chars
 
 """
 
