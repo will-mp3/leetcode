1ed4e7b6d7a2c9647e0065ee964134f4c718f117
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-21 13:50:10 -0500

    added code for 155

--- MEDIUM/155_MinStack.py
+++ MEDIUM/155_MinStack.py
@@ -11,7 +11,32 @@ int getMin() retrieves the minimum element in the stack.
 You must implement a solution with O(1) time complexity for each function.
 """
 
+class MinStack:
 
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        # use append
+        # we are storing the values in the stack along with the minimum value at that point
+
+        # if stack is empty, val is the min val
+        if not self.stack:
+            self.stack.append((val, val))
+            return
+        
+        current_min = self.stack[-1][1] # update current min based on top value
+        self.stack.append((val, min(val, current_min))) # when adding this new value, update its min compared to the current min
+
+    def pop(self) -> None:
+        # use pop
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0] # top elements value
+
+    def getMin(self) -> int:
+        return self.stack[-1][1] # top elements min
 
 """
 
