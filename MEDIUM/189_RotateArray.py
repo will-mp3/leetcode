25eb7834a4e4765a82ec4a36033e747fea8a2fa3
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-21 12:27:54 -0500

    added code for 189

--- MEDIUM/189_RotateArray.py
+++ MEDIUM/189_RotateArray.py
@@ -2,7 +2,25 @@
 Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
 """
 
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        # solution for O(n) time, O(n) space
+        n = len(nums)
+        a = [0] * n
 
+        for i in range(n):
+            a[(i + k) % n] = nums[i]
+
+        nums[:] = a
+
+        # solution for O(1) space, O(n * k) time
+        n = len(nums)
+        k %= n # for when k is greater than len(nums), saves us from O(n^2)
+
+        for i in range(k):
+            tmp = nums[-1]
+            for j in range(n):
+                nums[j], tmp = tmp, nums[j]
 
 """
 
