cb070104877edcca98aa921a0c2376c86bafc572
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-22 10:21:39 -0500

    added code for 2

--- MEDIUM/2_AddTwoNumbers.py
+++ MEDIUM/2_AddTwoNumbers.py
@@ -6,7 +6,29 @@ Add the two numbers and return the sum as a linked list.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 """
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(0)
+        cur = dummyHead
+        carry = 0
 
+        while l1 or l2 or carry:
+            x = l1.val if l1 else 0
+            y = l2.val if l2 else 0
+            lsum = x + y + carry
+            carry = lsum // 10
+            node = ListNode(lsum % 10)
+            cur.next = node
+            cur = node
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+        
+        return dummyHead.next
 
 """
 
