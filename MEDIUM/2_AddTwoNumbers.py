fcabd74d5ad437e2870608e1abe643f6c4062756
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-22 10:32:31 -0500

    added explanation for 2

--- MEDIUM/2_AddTwoNumbers.py
+++ MEDIUM/2_AddTwoNumbers.py
@@ -11,7 +11,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummyHead = ListNode(0)
@@ -32,5 +32,15 @@ class Solution:
         return dummyHead.next
 
 """
-
+to solve this problem we use basic arithmetic in combination with a carry variable.
+we know that due to resizing of lists we cant edit our current lists in place, we must make a new one.
+we start by creating a head node, dummyHead, and setting cur equal to it along with our carry value equal to 0
+while l1, l2, or carry are not Null or zero we loop through our lists.
+first, we check if l1 is not null; if l1 exists we set x equal to its value otherwise we set x equal to 0.
+we do the same for y and l2 and then add them together with our carry variable in our variable lsum.
+we then calculate our carry by dividing lsum by 10 (this type of division eliminates remainders).
+now that everything is calculated, we create a node node with our sum % 10 so account for the carry value.
+we set cur.next to the node to link it with our list then set cur equal to our node.
+finally we move l1 and l2 if their next value is not null.
+once finished we return dummyHead.next (the start of our list).
 """
