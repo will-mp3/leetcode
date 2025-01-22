6a57a14509feb3068e3b8abcd02f9bce7718bee2
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-22 10:24:14 -0500

    added listnode class

--- MEDIUM/2_AddTwoNumbers.py
+++ MEDIUM/2_AddTwoNumbers.py
@@ -7,10 +7,11 @@ You may assume the two numbers do not contain any leading zero, except the numbe
 """
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
         dummyHead = ListNode(0)
