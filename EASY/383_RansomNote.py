bd7b9276d9db9382578c3dfc1789802b7647945e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-22 12:39:21 -0500

    added code for 383

--- EASY/383_RansomNote.py
+++ EASY/383_RansomNote.py
@@ -5,7 +5,20 @@ return true if ransomNote can be constructed by using the letters from magazine 
 Each letter in magazine can only be used once in ransomNote.
 """
 
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if len(ransomNote) > len(magazine): return False
+        
+        # use the collections import to map characters and their counts
+        magazine_counts = collections.Counter(magazine)
+        ransom_counts = collections.Counter(ransomNote)
 
+        for char, count in ransom_counts.items():
+            magazine_count = magazine_counts[char]
+            if magazine_count < count:
+                return False
+            
+        return True
 
 """
 
