c8e99d53d34abb98b153574e1772fa45d65fafe9
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-23 12:42:19 -0500

    added explanation for 45

--- MEDIUM/45_JumpGameII.py
+++ MEDIUM/45_JumpGameII.py
@@ -17,7 +17,7 @@ class Solution:
         res, n = 0, len(nums)
 
         for i in range(n - 1):
-            far = max (far, i + nums[i])
+            far = max(far, i + nums[i])
 
             if i == end:
                 res += 1
@@ -26,5 +26,17 @@ class Solution:
         return res
 
 """
-
+to solve this problem we take a greedy approach, we ultimately just need to calculate the smallest amount of steps, not the optimal steps.
+we have two variables we track, far and end, 
+which track the farthest point we can reach from our current position and the end point we have reached.
+we iterate through the entire list, each time calculating a new farthest index.
+after this, if our current index i equals our current endpoint we increment result by 1 and set our new endpoint equal to the farthest index.
+what this is doing is finding the farthest point we can reach at our current index.
+for example is index 2 has the value 3 and index 3 has the value 1 we keep that farthest distance from index 2.
+using this farthest distance we update our result and end point every time we reach it.
+the first iteration end automatically updates to the farthest point, and until we reach that now saved end point the farthest point keeps updating.
+once we reach the endpoint, our farthest point has updated to the max of any position thus far and gets updated as our new endpoint.
+once it gets large enough to cross the end threshhold then i will never reach it, and result will stop incrementing.
+the loop finishes and result is returned.
+this soltution runs in O(n) linear time.
 """
