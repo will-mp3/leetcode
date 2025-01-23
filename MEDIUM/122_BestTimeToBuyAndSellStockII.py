a4b55b5906884e75100ec559b9d8a969f5162fb7
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-23 11:00:44 -0500

    added code for 122

--- MEDIUM/122_BestTimeToBuyAndSellStockII.py
+++ MEDIUM/122_BestTimeToBuyAndSellStockII.py
@@ -8,7 +8,27 @@ However, you can buy it then immediately sell it on the same day.
 Find and return the maximum profit you can achieve.
 """
 
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        i = 0
+        peak, valley = prices[0], prices[0]
+        profit = 0
+            
+        while i < len(prices) - 1:
 
+            while i < len(prices) - 1 and prices[i] >= prices[i + 1]:
+                i += 1
+            
+            valley = prices[i]
+
+            while i < len(prices) - 1 and prices[i] <= prices[i + 1]:
+                i += 1
+            
+            peak = prices[i]
+
+            profit += peak - valley
+        
+        return profit
 
 """
 
