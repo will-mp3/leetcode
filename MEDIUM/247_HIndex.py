5a443d3a88fda5f4221edccd0a3d27d77e9511d8
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-23 15:02:34 -0500

    added code for 247

--- MEDIUM/247_HIndex.py
+++ MEDIUM/247_HIndex.py
@@ -7,7 +7,23 @@ The h-index is defined as the maximum value of h such that the given researcher 
 that have each been cited at least h times.
 """
 
-
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        papers = [0] * (n + 1)
+        
+        # Counting papers for each citation number
+        for c in citations:
+            papers[min(n, c)] += 1
+        
+        # Finding the h-index
+        k = n
+        s = papers[n]
+        while k > s:
+            k -= 1
+            s += papers[k]
+        
+        return k
 
 """
 
