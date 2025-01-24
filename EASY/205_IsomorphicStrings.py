666c22bff7229a0038b72b4d6ac5fa231adb3f42
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-24 17:25:57 -0500

    added explanation for 205

--- EASY/205_IsomorphicStrings.py
+++ EASY/205_IsomorphicStrings.py
@@ -26,5 +26,14 @@ class Solution:
         return True
 
 """
-
+for this solution we make use of a multiple dicitonary mapping technique and the zip function in python.
+we want to map the characters in both strings as we come across them and compare accordingly as we go.
+to do this we initiate t to s and s to t dictionaries and iterate through the characters in s and t.
+to do this we use the zip function, which allows us to combine iterables into a tuple-like form and traverse them side by side.
+we go through the characters in s and t and check two cases.
+the first being if the characters are not mapped in BOTH dictionaries, if this is the case we map them to both dicts.
+the second case is when a mapping dosent exist or dosent map.
+we check to see if the value mapped to key c1 equals c2 and vise verse.
+if this ever executes false is immediately returned, we found uneven or incorrect mapping.
+if the entire traversal runs we return True.
 """
