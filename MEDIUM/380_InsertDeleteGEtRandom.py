f59bf2a0912f85cc2c579295f7e66e250b1bb8b3
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-24 16:36:05 -0500

    added code for 380

--- MEDIUM/380_InsertDeleteGEtRandom.py
+++ MEDIUM/380_InsertDeleteGEtRandom.py
@@ -14,7 +14,34 @@ Each element must have the same probability of being returned.
 You must implement the functions of the class such that each function works in average O(1) time complexity.
 """
 
+class RandomizedSet:
 
+    def __init__(self):
+        self.dict = {}
+        self.list = []
+
+    def insert(self, val: int) -> bool:
+        # add value : index into dict, append value to array
+        if val in self.dict:
+            return False
+        self.dict[val] = len(self.list) # mapping element to its index in our array, notice order
+        self.list.append(val)
+        return True
+
+    def remove(self, val: int) -> bool:
+        # retrieve an idex to delete from dict, swap last element to that position, pop last element
+        if val in self.dict:
+            # move last element to the index of element to delete
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            self.list.pop()
+            del self.dict[val]
+            return True
+        return False
+
+    def getRandom(self) -> int:
+        # implemented with the help of random.choice module
+        return choice(self.list)
 
 """
 
