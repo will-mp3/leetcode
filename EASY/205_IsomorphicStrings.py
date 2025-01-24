2567a5c324e44402124a6fa8c5623c6cbb41e91e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-24 17:20:22 -0500

    added code for 205

--- EASY/205_IsomorphicStrings.py
+++ EASY/205_IsomorphicStrings.py
@@ -7,7 +7,23 @@ All occurrences of a character must be replaced with another character while pre
 No two characters may map to the same character, but a character may map to itself.
 """
 
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # define dictionaries to map characters in t to s and characters in s to t
+        maps2t = {}
+        mapt2s = {}
 
+        for c1, c2 in zip(s, t):
+            # case 1: check if characters are mapped already, if not map them into both dicts
+            if c1 not in maps2t and c2 not in mapt2s:
+                maps2t[c1] = c2
+                mapt2s[c2] = c1
+            
+            # case 2: mapping dosent exist or mapping dosent match
+            elif maps2t.get(c1) != c2 or mapt2s.get(c2) != c1:
+                return False
+            
+        return True
 
 """
 
