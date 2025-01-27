08baebacc246996e9b68d8d308e92d5dc11d6311
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-01-27 11:17:15 -0500

    added code for 228

--- EASY/228_SummaryRanges.py
+++ EASY/228_SummaryRanges.py
@@ -13,7 +13,22 @@ Each range [a,b] in the list should be output as:
 "a" if a == b
 """
 
+class Solution:
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        i = 0
+        res = []
 
+        while i < len(nums):
+            start = nums[i]
+            while i + 1 < len(nums) and nums[i] + 1 == nums[i + 1]:
+                i += 1
+            if start != nums[i]:
+                res.append(str(start) + "->" + str(nums[i]))
+            else:
+                res.append(str(nums[i]))
+            i += 1
+        
+        return res
 
 """
 
