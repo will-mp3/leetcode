f323f8f7d87b8c8c0b475e7ec9847ed3cec9b0e2
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-13 14:59:57 -0500

    fixed desc for 78

--- MEDIUM/78_Subsets.py
+++ MEDIUM/78_Subsets.py
@@ -1,7 +1,8 @@
 """
 Given an integer array nums of unique elements, return all possible subsets (the power set).
 
-The solution set must not contain duplicate subsets. Return the solution in any order.
+The solution set must not contain duplicate subsets. 
+Return the solution in any order.
 """
 
 
