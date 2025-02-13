96b53c739b0cfe9919b8529680df3d0a64099c7a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-13 12:51:23 -0500

    added explanation for 22

--- MEDIUM/22_GenerateParentheses.py
+++ MEDIUM/22_GenerateParentheses.py
@@ -34,5 +34,19 @@ class Solution:
         return res
 
 """
-
+this solution makes use of a recursive function that traverses a decision tree.
+to envision this, imagine that we have three rules to follow:
+only add open parenthesis if open count < n
+only add a closed parentheses if closed < open
+only valid if open == closed == n
+we must traverse our decision tree following these rules to discover each valid set of parentheses where open == closed == n.
+this is also conveniently our base case.
+we keep a stack to track our parentheses and start by calling our recursive function with the open and closed count equal to zero.
+our base case is when the open count, closed count, and n are all equal; at that point join the stack and append to result.
+if that is not the case we begin making recursive calls based on two conditions:
+if the open count is less than n we add an open parenthesis to the stack and call our function with 1 added to the open count.
+this branches off and continues until the base case is found, we add a line to pop the stack after so its clear for the next combination.
+if the closed count is less than the open count we can add a closed parenthesis to the stack.
+we call our function like before but this time adding one to the closed count.
+when all is said and done we will have all of the valid combinations saved to result and can return it.
 """
