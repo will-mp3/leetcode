4cb6058127fc9c19eb75da607adae3f9cd001791
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-13 15:22:10 -0500

    added explanation for 78

--- MEDIUM/78_Subsets.py
+++ MEDIUM/78_Subsets.py
@@ -29,5 +29,14 @@ class Solution:
         return res
 
 """
-
+this solution makes use of the backtracking technique and a depth first search algorithm.
+the reason we use dfs is because we are approaching this problem in a binary decision tree manner.
+we build our subsets based on two decisions, the left path adds nums[i] to the current subset and the right path adds nothing.
+this is exemplified in our dfs function.
+our function works by first checking our base case, if i (the index of the value in nums) is out of bounds,
+we append the current subset to result and return.
+we are given two decisions to make, the first being to add nums[i] to our subset.
+for this decision we append nums[i] to subset and call our dfs function incrementing i.
+for the decision not to include nums[i], we pop the just added nums[i] and call dfs incrementing i as well.
+we start by calling dfs with i set to zero, once the recursion has completed we return our result.
 """
