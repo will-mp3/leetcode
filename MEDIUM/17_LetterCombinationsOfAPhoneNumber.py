cd280f7ddd3668e759444ced5df2239c96a57716
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-13 14:54:40 -0500

    added explanation for 17

--- MEDIUM/17_LetterCombinationsOfAPhoneNumber.py
+++ MEDIUM/17_LetterCombinationsOfAPhoneNumber.py
@@ -34,5 +34,16 @@ class Solution:
         return res
 
 """
-
+this solution makes use of an imbedded recrusive function and the backtracking technique.
+notice that we must hardcode a hashmap with the letter character mappings, this is ugly but unavoidable.
+the recursive function is rather simple.
+the base case is when our current strings length is equal to that of digits. 
+this is because we are looking for encoded combos so three digits could never yield more than three characters.
+while this base case is not true we iterate through all of the possible characters for each digit in digits.
+we iterate through digits usng variable i which is incremented with each recursive call.
+what this is essentially doing is for every character of every digit given.
+for example, if our first digit (digits[0]) was 2, then we would look through all characters mapped to digits[0].
+this idea continues as we move i with each succesive recursive call.
+ultimately the length of digits will be hit and we append the current string which we have been updating with each c in the loop.
+we call backtrack only if digits exists starting with i = 0 and curStr empty then return our res once the recursive stops.
 """
