f7b03c5574eefdd48d48d52813bafb7770474333
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-13 15:13:17 -0500

    added code for 78

--- MEDIUM/78_Subsets.py
+++ MEDIUM/78_Subsets.py
@@ -5,7 +5,28 @@ The solution set must not contain duplicate subsets.
 Return the solution in any order.
 """
 
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
 
+        subset = []
+        def dfs(i): # i is the index of the value we are making a decision on
+            # base case
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            
+            # two decisions
+            # decision to include nums[i], left branch of decision tree
+            subset.append(nums[i])
+            dfs(i + 1)
+
+            # decision not to include nums[i]
+            subset.pop()
+            dfs(i + 1)
+        
+        dfs(0)
+        return res
 
 """
 
