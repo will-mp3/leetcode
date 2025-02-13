e59ae1cdbdcf5303c7b50576ab91b96f6d57b8ef
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-13 12:35:59 -0500

    added code for 22

--- MEDIUM/22_GenerateParentheses.py
+++ MEDIUM/22_GenerateParentheses.py
@@ -2,7 +2,36 @@
 Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
 """
 
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        """
+        only add open parenthesis if open count < n
+        only add a closed parentheses if closed < open
+        only valid if open == closed == n
+        """
 
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN): # nested recursive function
+            # base case
+            if openN == closedN == n:
+                res.append("".join(stack)) # taking the values in our stack and joining them
+                return
+
+            # the following two conditionals effectively check every valid branch in the decision tree
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop() # every time we're done with backtracking we pop the just added character
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return res
 
 """
 
