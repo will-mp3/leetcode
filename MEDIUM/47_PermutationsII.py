b3a7daed813faf9026bf0bcd6dd126167c4328b1
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-17 15:24:59 -0500

    added explanation for 47

--- MEDIUM/47_PermutationsII.py
+++ MEDIUM/47_PermutationsII.py
@@ -30,5 +30,16 @@ class Solution:
         return res
 
 """
-
+this problem is similar to the original permutations question, except this time our input array may contain duplicates.
+this prevents us from backtracking using a standard decision tree like in the first version of the problem.
+we still use the backtracking technique and a recursive dfs function, but there is required preprocessing.
+namely, we must turn our input array into a hashmap of unique numbers and their respective counts.
+this allows us to both avoid duplicate decisions and ensure all values are included in our permutations.
+once the reprocessing is done we call our depth first search function which is imbedded.
+the way this works is by first checking the base case, if the current permutation (perm) is equal in length to our input array.
+if this is true we append a copy and return.
+if our base case does not execute we then iterate through all our values in count.
+for each value n whose count is greater than zero we append n to our current permuation, decrement count, and call our dfs function again.
+this process repeats until the recrusive stack is popped and then we cleanup by reincrementing count and popping the added n (for each call).
+once the recrusion is finishing we return the result.
 """
