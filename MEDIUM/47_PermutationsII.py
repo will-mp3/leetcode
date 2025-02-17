a2734811456e1d5728e624f59efba20837606d7d
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-17 15:18:32 -0500

    added code for 47

--- MEDIUM/47_PermutationsII.py
+++ MEDIUM/47_PermutationsII.py
@@ -2,7 +2,32 @@
 Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
 """
 
-
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res = [] # store list of permutations
+        perm = [] # list ot store each permutation
+        count = { n:0 for n in nums } # count hashmap mapping every n in nums with its count
+        for n in nums:
+            count[n] += 1
+        
+        def dfs():
+            # base case
+            if len(perm) == len(nums):
+                res.append(perm.copy())
+                return
+            
+            for n in count:
+                if count[n] > 0:
+                    perm.append(n)
+                    count[n] -= 1
+                    # recurse
+                    dfs()
+                    # cleanup
+                    count[n] += 1
+                    perm.pop()
+    
+        dfs()
+        return res
 
 """
 
