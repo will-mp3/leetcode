ba54e3d08d5590f277a602901db0401c1705e9e5
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-17 16:38:24 -0500

    added explanation for 210

--- MEDIUM/210_CourseScheduleII.py
+++ MEDIUM/210_CourseScheduleII.py
@@ -46,5 +46,23 @@ class Solution:
         return output
 
 """
-
+this solution makes use of the topological sort algorithm.
+this algorithm makes use of depth first search as well as an adajaceny list to find the correct order in which courses must be taken.
+the first thing we need to do is making our prereq adjacency list which contains course:[prereqs] pairs.
+once this is created we initialize our output list and visit & cycle sets.
+as previously mentioned we make use of depth first search in this solution so we will need to create the function for that as well.
+the way this function works is by first checking two condintions.
+the first is if our course is in our cycle set, if this is true we have found a cycle and must return false.
+the second condition is is our course is in visit, if this is true we can return true and continue, no need to deal with it twice.
+if we get through these tow conditions we add our course to our current cycle set.
+next, we got hrough all of the prereqs for our current course, 
+we call our dfs function on them and if False is returned ever we return False, a cycle was found.
+if false is never returned we can begin cleaning up our current process.
+we remove the current course from our cycle set, add it to our visit set, and append it to our output set.
+finally we can return true at the end of our dfs function.
+now that our depth first search function is created we need to call it.
+for every course in our courseList (we use the range of numCourses since theyre represented as integers) we call our dfs function.
+again if this ever returns false we know a cycle was found and must return an empty list.
+if false is never returned our loop will finish and we can return out output list.
+this solution runs in O(e + v) where e is the number of edges and v is the number of vertexes.
 """
