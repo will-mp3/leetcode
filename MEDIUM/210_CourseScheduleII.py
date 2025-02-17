ee0546516d4f14e7c54695f65b9d553afb85535d
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-17 16:09:13 -0500

    added code for 210

--- MEDIUM/210_CourseScheduleII.py
+++ MEDIUM/210_CourseScheduleII.py
@@ -9,7 +9,41 @@ If there are many valid answers, return any of them.
 If it is impossible to finish all courses, return an empty array.
 """
 
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        # build adjacency list of prereqs
+        prereq = { c:[] for c in range(numCourses) }
+        for crs, pre in prerequisites:
+            prereq[crs].append(pre)
 
+        # a course has three possible states
+        # visited -> crs has been added to output
+        # visiting -> crs not added to output but added to cycle
+        # unvisited -> crs not added to output or cycle
+
+        output = []
+        visit, cycle = set(), set()
+
+        def dfs(crs):
+            # check for cycle
+            if crs in cycle:
+                return False
+            if crs in visit:
+                return True
+            
+            cycle.add(crs)
+            for pre in prereq[crs]:
+                if not dfs(pre):
+                    return False
+            cycle.remove(crs)
+            visit.add(crs)
+            output.append(crs)
+            return True
+        
+        for c in range(numCourses):
+            if not dfs(c):
+                return []
+        return output
 
 """
 
