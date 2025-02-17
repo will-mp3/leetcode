6e62c7620af63ae5c7f3192550a698042e08ff7a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-17 17:16:23 -0500

    added code for 146

--- MEDIUM/146_LRUCache.py
+++ MEDIUM/146_LRUCache.py
@@ -11,7 +11,55 @@ If the number of keys exceeds the capacity from this operation, evict the least 
 The functions get and put must each run in O(1) average time complexity.
 """
 
+class Node:
+    def __init__(self, key, val):
+        self.key, self.val = key, val
+        self.prev = self.next = None
 
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {} # map key to node
+
+        # left = least recent, right = most recent
+        self.left, self.right = Node(0, 0), Node(0, 0)
+        self.left.next, self.right.prev = self.right, self.left
+
+    # helper functions remove and insert
+    # remove from linked list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next, nxt.prev = nxt, prev
+    
+    # insert at right
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = nxt.prev = node
+        node.next, node.prev = nxt, prev
+        
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            # update most recent
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+
+            return self.cache[key].val # keys are mapped to nodes
+        return -1 # default return value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.remove(self.cache[key])
+        # create and insert new node
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        # check capacity
+        if len(self.cache) > self.cap:
+            # remove LRU from list and delete from cache
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
 
 """
 
