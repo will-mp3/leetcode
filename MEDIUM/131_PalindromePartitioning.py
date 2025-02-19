9810689fd976d372bbb6fef9b2e4332be5653440
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-18 19:01:59 -0500

    added code for 131

--- MEDIUM/131_PalindromePartitioning.py
+++ MEDIUM/131_PalindromePartitioning.py
@@ -3,7 +3,34 @@ Given a string s, partition s such that every substring of the partition is a pa
 Return all possible palindrome partitioning of s.
 """
 
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        res = []
+        part = [] # current partition
 
+        def dfs(i):
+            # base case
+            if i >= len(s):
+                res.append(part.copy())
+                return
+            
+            for j in range(i, len(s)):
+                # check if palindrome using helper function
+                if self.isPali(s, i, j): # if palindrome, add to res. otherwise skip to next iteration
+                    part.append(s[i:j+1])
+                    dfs(j + 1)
+                    #cleanup
+                    part.pop()
+        
+        dfs(0)
+        return res
+        
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            l, r = l + 1, r - 1
+        return True
 
 """
 
