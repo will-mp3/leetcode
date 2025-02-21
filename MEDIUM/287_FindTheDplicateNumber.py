f793fbd8f946f8caa4bb545f6cbc9490074004ef
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-21 17:12:28 -0500

    added code for 287

--- MEDIUM/287_FindTheDplicateNumber.py
+++ MEDIUM/287_FindTheDplicateNumber.py
@@ -6,7 +6,25 @@ There is only one repeated number in nums, return this repeated number.
 You must solve the problem without modifying the array nums and using only constant extra space.
 """
 
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        slow, fast = 0, 0
+        # phase one: find where slow and fast intersect
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]] # advance fast by 2
 
+            if slow == fast:
+                break
+
+        slow2 = 0
+        # phase two: find where slow and slow2 intersect and return
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            
+            if slow == slow2:
+                return slow
 
 """
 
