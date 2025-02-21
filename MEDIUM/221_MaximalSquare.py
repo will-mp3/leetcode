dd6904250b5ee54cf389cc667a7a939a775d0ac0
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-21 16:06:48 -0500

    added explanation for 221

--- MEDIUM/221_MaximalSquare.py
+++ MEDIUM/221_MaximalSquare.py
@@ -29,5 +29,21 @@ class Solution:
         return max(cache.values()) ** 2
 
 """
-
+this problem can be solved effeciently either using recursion or dyanmic programming.
+the idea is we must go through unvisited cells in a 2d array and mark them based on the amount of neighboring ones.
+dynamic programming takes a bottom up approach whereas recursion takes a top down approach.
+for this solution we will opt for the recursive implementation.
+the idea is that we go through each unvisited cell, starting top left, and check its right, diagonal, and lower neighbors.
+with each check, we increment the value held in the cache based on its maximum length (we will square this at the end).
+to implement this we will define a helper function which I'll explain later.
+to start we must define our cache which will be represented by a hash table, we are mapping our (r,c) positions to their max square length.
+our helper variable takes in a starting position and calls itself recursively, we start at 0,0.
+the first thing we check is the base case, which happens when either of our coordinates are out of bound.
+if the base case dosent execute and the current cell is unvisited, we call our helper function on our right, diagonal, and lower neighbor.
+this will traverse the entire matrix and get us to the bottom right position where the recursive call stack will begin returning.
+now we map the current cell in the cache to zero, and check if it contains a 1 in our original matrix.
+if the cell contains a 1, we map 1 + the minimum of its three neighbors to our cache.
+what this is doing is checking to see that all its square neighbors also contain ones, and if their neighbors contains ones and so on.
+once the recursion finishes we return our finished cache and return the max value from it squared.
+this solution runs in O(m * n) time where m is the number of rows and n is the number of columns.
 """
