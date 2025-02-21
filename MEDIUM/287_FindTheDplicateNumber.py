250bf91f486ea35c1ee0007d4335354c464c500a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-21 17:35:04 -0500

    added explanation for 287

--- MEDIUM/287_FindTheDplicateNumber.py
+++ MEDIUM/287_FindTheDplicateNumber.py
@@ -32,4 +32,12 @@ to use our array as a linked list we treat the values in each position as pointe
 for example if index zero held a 3, then it would point to index 3.
 say index three contains a 2, we would map a node with value 3 pointing to a node with value 2 and so on.
 these indexes and nodes are important to understand for how we will increment our pointers in the algorithm.
+our goal is to find a cycle, specifically the beginning of one, as that will be the value that has a duplicate.
+for this we will use floyd's algorithm which is capable of not only finding cycles but finding the start of them.
+floyd's algorithm is split into two phases, one where our fast and slow pointer intersect and one where two slow pointers intersect.
+for phase one we increment our slow pointer by shifting its pointer one node, and we increment our fast pointer by shifting it two.
+notice the syntax for this, its different than index shifting since we are hopping spots based on node values.
+once they equal we break our loop and keep slow where it is.
+for phase two we create a new slow pointer and loop once again until they intersect, this time with equal increments.
+once they intersect we have found the start of our cycle and can return either pointer.
 """
