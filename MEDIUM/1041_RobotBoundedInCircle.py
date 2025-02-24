a2b9cb153e20da262b18a9cb3e8337282f81f600
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-23 20:42:37 -0500

    added code for 1041

--- MEDIUM/1041_RobotBoundedInCircle.py
+++ MEDIUM/1041_RobotBoundedInCircle.py
@@ -15,7 +15,21 @@ The robot performs the instructions given in order, and repeats them forever.
 Return true if and only if there exists a circle in the plane such that the robot never leaves the circle.
 """
 
-
+class Solution:
+    def isRobotBounded(self, instructions: str) -> bool:
+        dirX, dirY = 0, 1
+        x, y = 0, 0
+        
+        for d in instructions:
+            if d == "G":
+                x, y = x + dirX, y + dirY       
+            elif d =="L":
+                dirX, dirY = -1*dirY, dirX
+            else: # d == "R"
+                dirX, dirY = dirY, -1*dirX
+        
+        # return true if position did not change OR the direction changed from the original
+        return (x, y) == (0, 0) or (dirX, dirY) != (0, 1)
 
 """
 
