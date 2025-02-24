9d07ce70aa23debfff8de682122fa5df81361efa
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-24 16:22:55 -0500

    added code for 36

--- MEDIUM/36_ValidSudoku.py
+++ MEDIUM/36_ValidSudoku.py
@@ -11,7 +11,30 @@ A Sudoku board (partially filled) could be valid but is not necessarily solvable
 Only the filled cells need to be validated according to the mentioned rules.
 """
 
+import collections
 
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set) # key = (row // 3, col // 3)
+
+        for r in range(9):
+            for c in range(9):
+                # check for empty space
+                if board[r][c] == ".":
+                    continue
+                
+                if (board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
 
 """
 
