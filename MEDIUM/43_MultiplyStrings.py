2490d4f3909273aab31fbd6066975f2f35f2107a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-24 17:49:14 -0500

    added explanation for 43

--- MEDIUM/43_MultiplyStrings.py
+++ MEDIUM/43_MultiplyStrings.py
@@ -33,5 +33,19 @@ class Solution:
         return "".join(res)
 
 """
-
+this problem asks us to multiple strings without the use of built in libraries to convert the string directly.
+to accomplish this we make use of old-school integer multiplication, each position multiplied by each position with carry values etc.
+to start we check the edge case where a zero is given.
+once thats out of the way we allocate our result array and reverse both of our number arrays for ease of arithmetic.
+next we next a for loop to go through all digits in num1 and num2, this will allow that multiplication mentioned prior.
+for each iteration we calculate our digit by multiplying the numbers at our current i1 and i2 indices.
+once we get our digit we go through three steps:
+first we add our digit value to the index i1 + i2.
+next we add any carry values to the following index using integer division by 10.
+lastly, we set that original i1 + i2 index equal to the current value at its position mod 10.
+these steps accoutn for carry values, double digit values, past carrys, etc.
+once the nested loop completes we can reverse our result list.
+note that result may possess leading zeroes once reversed, we will use a beggining pointer to accomodate for this.
+we increment our beg pointer until there are no more leading zeroes then turn our result into a list of strings using map.
+once done we can join our result array to one string using the join operation.
 """
