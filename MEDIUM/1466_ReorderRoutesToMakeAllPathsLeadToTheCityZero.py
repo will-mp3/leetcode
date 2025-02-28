7a0bd7bad0abfa405f5b5fcffe6ffc86cae7387c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-28 18:59:39 -0500

    added code for 1466

--- MEDIUM/1466_ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
+++ MEDIUM/1466_ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
@@ -12,7 +12,39 @@ Your task consists of reorienting some roads such that each city can visit the c
 It's guaranteed that each city can reach city 0 after reorder.
 """
 
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        # start at city 0
+        # recursively check its neighbors
+        # count outgoing edges
 
+        edges = { (a,b) for a, b in connections }
+        neighbors = { city:[] for city in range(n) }
+        visit = set()
+        change = 0
+
+        # fill neighbors hashmap
+        for a, b in connections:
+            neighbors[a].append(b)
+            neighbors[b].append(a)
+
+        def dfs(city):
+            nonlocal edges, neighbors, visit, change
+
+            for neighbor in neighbors[city]:
+                if neighbor in visit:
+                    continue
+                
+                # check if this neighbor can reach city zero
+                if (neighbor, city) not in edges:
+                    change += 1
+                visit.add(neighbor)
+                dfs(neighbor) # check if neighbor's neighbor can reach city zero
+
+        visit.add(0)
+        dfs(0)
+
+        return change
 
 """
 
