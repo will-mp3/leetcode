71c1225a7a9d0ff500dce946528f751d7cc33c46
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-02-28 19:53:40 -0500

    added explanation for 1466

--- MEDIUM/1466_ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
+++ MEDIUM/1466_ReorderRoutesToMakeAllPathsLeadToTheCityZero.py
@@ -47,5 +47,19 @@ class Solution:
         return change
 
 """
-
-"""
+this problem is based around graph comprehension.
+we know that we are given n cities and n - 1 roads which are connected as defined by our connections list.
+we can view the citys as nodes and the roads as edges and use an adjacency list in the form of a hash map to track them.
+we want to count the amount of cities which are not connected to their neighbors and return that value.
+we can accomplish this using our graph of nodes and a depth first search algorithm to recursively check each node.
+each iteration we check to see of our node and its neighbors are connected, and if not we add 1 to our count.
+to start we define our edges hashmap which contains the links between nodes.
+we also make a neighbors hashmap which maps the city (node) to its neighbors.
+we also keep track of a visit set to avoid repeated work.
+once made we create our dfs function, we go through each node in neighbors, first checking if its been visited.
+if so, we continue to the next iteration.
+otherwise we check if the neighbor and our current city are a present edge, if not we increment our counter variable.
+every iteration we add our neighbor to the visted set and call our dfs function on the current neighbor to continue searching the graph.
+we start at city 0 and return our counter variable change once the dfs completes.
+this solution runs in O(n) linear time.
+""" 
