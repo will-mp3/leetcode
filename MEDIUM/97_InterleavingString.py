826de0e69f64fbfeb05c5f1a16b167397c61c42f
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-03 20:12:52 -0500

    added code for 97

--- MEDIUM/97_InterleavingString.py
+++ MEDIUM/97_InterleavingString.py
@@ -11,7 +11,25 @@ The interleaving is s1 + t1 + s2 + t2 + s3 + t3 + ... or t1 + s1 + t2 + s2 + t3 
 Note: a + b is the concatenation of strings a and b.
 """
 
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
 
+        # initialize 2 dimensional dynamic programming array 
+        dp = [ [False] * (len(s2) + 1) for i in range((len(s1) + 1))]
+        # initalize bottom right corner value as True
+        dp[len(s1)][len(s2)] = True
+
+        # work our way left then up row by row from bottom right corner
+        for i in range(len(s1), -1, -1):
+            for j in range(len(s2), -1, -1):
+                if i < len(s1) and s1[i] == s3[i + j] and dp[i + 1][j]:
+                    dp[i][j] = True
+                if j < len(s2) and s2[j] == s3[i + j] and dp[i][j + 1]:
+                    dp[i][j] = True
+
+        return dp[0][0]
 
 """
 
