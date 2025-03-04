4b239a96800c9159800089541b56164af8da1dc4
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-04 10:06:13 -0500

    added explanation for 1963

--- MEDIUM/1963_MinimumNumberOfSwapsToMakeTheStringBalanced.py
+++ MEDIUM/1963_MinimumNumberOfSwapsToMakeTheStringBalanced.py
@@ -26,5 +26,14 @@ class Solution:
         return (maxClose + 1) // 2
 
 """
-
+unlike a lot of the previous balanced string problems where a stack was used, this time we take a different approach.
+this problem only asks us to return the minimum amount of swaps needed to balance and gurantees there is an even number of bracket types.
+knowing this our approach is very simple, we will use one pass to count the amount of closed brackets.
+specfically we will count and track the max amount of excess closed brackets to open brackets.
+for example if we have count through four brackets and three of them are closed then we know we have an excess of 2.
+we use the variable close to track and increment/decrement this and maxClose to keep track of the max amount.
+we run through the characters in s, decrementing close when we find an open bracket and incrementing when we find a closed.
+each iteration we update our max close value and once finished we return maxClose + 1 // 2.
+this is because each swap actually removes two excess closed brackets, not one, so we account for that using integer division.
+this solution runs in O(n) linear time.
 """
