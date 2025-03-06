0d298edce167341cb5add1b199872ab2da4c0ba2
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-06 18:02:49 -0500

    added code for 120

--- MEDIUM/120_Triangle.py
+++ MEDIUM/120_Triangle.py
@@ -5,7 +5,15 @@ For each step, you may move to an adjacent number of the row below.
 More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
 """
 
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = [0] * (len(triangle) + 1)
 
+        for row in triangle[::-1]:
+            for i, n in enumerate(row): # enumerate to access value and index at same time
+                dp[i] = n + min(dp[i], dp[i + 1]) # overwrite position i with minimum of children below
+
+        return dp[0]
 
 """
 
