5bc3d8b3fc4fc738b35ae2611711858bc916fb3f
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-06 18:07:14 -0500

    added explanation for 120

--- MEDIUM/120_Triangle.py
+++ MEDIUM/120_Triangle.py
@@ -16,5 +16,15 @@ class Solution:
         return dp[0]
 
 """
-
+this problem is best visualized as a tree because the adjacent step mechanic is best compared to children in a tree.
+for example the first rows first element, row1[0], will have children row2[0] & row2[1] which are accessible.
+with that understood we want to approach this problem using dynamic programming.
+starting from the bottom we can calculate the minimum path for each value.
+we start by initializing an array of length triangle + 1 full of zeroes, this represents the row right below our last row.
+we iterate through every row in triangle, starting from the bottom, and every value in the row.
+for each value we check the minimum of its two children which are currently stored in our dp array.
+this array more specifically holds the shortest path of the children and is continually updated until it reachs the top, dp[0].
+the minimum of the current values child paths is used to overwrite the current value and the loop continues.
+this is done for every value and by the time it reaches the top we can return our top value, dp[0].
+this solutions runs in roughly O(n^2) time.
 """
