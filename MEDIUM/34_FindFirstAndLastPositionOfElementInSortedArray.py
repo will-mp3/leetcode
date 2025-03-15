30160b3165139d041a8e10e7aee51602fdb984ad
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-15 15:57:48 -0400

    added code for 34

--- MEDIUM/34_FindFirstAndLastPositionOfElementInSortedArray.py
+++ MEDIUM/34_FindFirstAndLastPositionOfElementInSortedArray.py
@@ -6,7 +6,37 @@ If target is not found in the array, return [-1, -1].
 You must write an algorithm with O(log n) runtime complexity.
 """
 
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        left = self.binarySearch(nums, target, True)
+        right = self.binarySearch(nums, target, False)
 
+        return [left, right]
+    
+    # leftBias = True/False; if False our res is rightBias (right index)
+    def binarySearch(self, nums, target, leftBias):
+        l, r = 0, len(nums) - 1
+        i = -1
+
+        while l <= r:
+            # calculate middle position
+            m = (l + r) // 2
+
+            # if target is bigger than our middle value, we know its right centered
+            if target > nums[m]:
+                l = m + 1
+            # if target is smaller than our middle value, we know its left centered
+            elif target < nums[m]:
+                r = m - 1
+            else:
+                i = m
+                # check which result index we are searching for
+                if leftBias:
+                    r = m - 1
+                else:
+                    l = m + 1
+
+        return i
 
 """
 
