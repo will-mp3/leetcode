b16b009f28d9d920e8f077dcf12516c1a3b3ecb6
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-27 08:55:58 -0400

    added explanation for 1448

--- MEDIUM/1448_CountGoodNodesInBinaryTree.py
+++ MEDIUM/1448_CountGoodNodesInBinaryTree.py
@@ -10,7 +10,7 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
-        
+
 class Solution:
     def goodNodes(self, root: TreeNode) -> int:
         
@@ -28,5 +28,17 @@ class Solution:
         return dfs(root, root.val)
 
 """
-
+for this problem, like many binary tree problems, we will make use of a recursive depth first search algorithm.
+the problem asks us to count the number of good nodes in the tree, requiring us to scan through all of the nodes in the tree by default.
+we obviously can use dfs to accomplish this, but we need some extra variables in order to evaluate each node.
+in order to check if a node is "good" we will keep track of a maxVal variable.
+maxVal represents the largest value seen in the current branch we are traversing.
+this will allow us to check each node to see if it qualifies as being good (no nodes in its path greater than itself).
+we create our dfs function and pass in the current node as well as the current max value for the branch.
+our base case is when our node is Null, in which we return zero.
+the first thing we do is check if the node is good or not, if it is we set our result variable to 1, otherwise set it to 0.
+this result variable is ultimately what we return, either 1 or 0 as a recursive incrementer.
+once we set result we can update our max value based on the current node and call dfs on the left and right children.
+once the recursive stack begins to pop, we return result and increment the result variables still in the stack (see dfs call).
+this solution runs in O(n) linear time.
 """
