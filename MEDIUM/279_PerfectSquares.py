d0b188779cc42b1cdcc1e4c944cfde2957ef5adc
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-30 13:02:41 -0400

    added code for 279

--- MEDIUM/279_PerfectSquares.py
+++ MEDIUM/279_PerfectSquares.py
@@ -5,7 +5,19 @@ A perfect square is an integer that is the square of an integer; in other words,
 For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 """
 
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [n] * (n + 1)
+        dp[0] = 0
 
+        for target in range(1, n + 1):
+            for s in range(1, target + 1):
+                square = s * s
+                if target - square < 0:
+                    break
+                dp[target] = min(dp[target], 1 + dp[target - square])
+
+        return dp[n]
 
 """
 
