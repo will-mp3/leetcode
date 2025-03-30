417c0f9b0dfa3bb3f4afd51d8b7bc6853c7c7f20
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-03-30 13:13:24 -0400

    added explanation for 279

--- MEDIUM/279_PerfectSquares.py
+++ MEDIUM/279_PerfectSquares.py
@@ -20,5 +20,20 @@ class Solution:
         return dp[n]
 
 """
-
+for this problem we use a bottom up dynamic programming algorithm.
+the idea is that instead of trying to find how many perfect squares make up n, we start at 0 and work our way backwards.
+for example if n = 12, we would instead start at n = 0, then n = 1, then n = 2, and so on.
+the minimum squares is stored in our dp array, whose index represents the above n values, and is repeatedly updated.
+what this does for us is create an array of minimum squares for each value.
+when we get to a new target we can check to see if there is a shorter path that we have already stored and update the array.
+to start we create a dp array of size n + 1 which is filled with our n value (n value is used so we can work with minimums later).
+we initiliaze position zero to 0 as our base case.
+next we iterate through every position (target) in our dp array, and for each target we iterate through values (s) from 1 to the target.
+for each iteration we calculate the square of our current s value.
+we check to see if the square is too large, if so we immediately break the inner loop and continue the outer loop.
+if the square is small enough we then check to update our dp array.
+to do this we check the minimum between the target indexes current element 
+and that of the target - square + 1 (since we have to include the current step).
+once the outer loop completes we can return the value stored at element n since our indexes represent our values.
+this solution runs in O(n * sqrt(n)).
 """
