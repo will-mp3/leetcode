2942b8500cb9bae0db88ee132fb5966381039b5b
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-04-22 16:57:38 -0400

    added file for 2323

---
+++ MEDIUM/2323_FindMinimumTimeToCompleteAllJobsII.py
@@ -0,0 +1,14 @@
+"""
+You are given two 0-indexed integer arrays jobs and workers of equal length, 
+where jobs[i] is the amount of time needed to complete the ith job, and workers[j] is the amount of time the jth worker can work each day.
+
+Each job should be assigned to exactly one worker, such that each worker completes exactly one job.
+
+Return the minimum number of days needed to complete all the jobs after assignment.
+"""
+
+
+
+"""
+
+"""
