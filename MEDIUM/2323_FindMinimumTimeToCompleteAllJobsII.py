cf716922054b1e725bce291280b3df8f8ddfbd1c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-04-22 16:57:51 -0400

    added code for 2323

--- MEDIUM/2323_FindMinimumTimeToCompleteAllJobsII.py
+++ MEDIUM/2323_FindMinimumTimeToCompleteAllJobsII.py
@@ -7,7 +7,21 @@ Each job should be assigned to exactly one worker, such that each worker complet
 Return the minimum number of days needed to complete all the jobs after assignment.
 """
 
+class Solution:
+    def minimumTime(self, jobs: List[int], workers: List[int]) -> int:
+        # brute force
+        n = len(jobs)
+        jobs.sort()
+        workers.sort()
+        res = float("-inf")
 
+        for i in range(n):
+            if jobs[i] % workers[i] == 0:
+                res = max(res, jobs[i] // workers[i])
+            else:
+                res = max(res,1 + jobs[i] // workers[i])
+
+        return res
 
 """
 
