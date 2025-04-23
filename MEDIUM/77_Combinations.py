d710629cc0d579a574eab692f864e7970d66f592
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-04-23 15:53:48 -0400

    added comments to 77

--- MEDIUM/77_Combinations.py
+++ MEDIUM/77_Combinations.py
@@ -8,14 +8,16 @@ class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
         
         def dfs(curr, first):
+            # if our current combination is of length k, add it to our result array
             if len(curr) == k:
                 ans.append(curr[:])
                 return
 
+            # for every value after (larger) then our current starting value, create a new combination
             for num in range(first, n + 1):
                 curr.append(num)
-                dfs(curr, num + 1)
-                curr.pop()
+                dfs(curr, num + 1) # once made, pass the combo into a new recursive call
+                curr.pop() # remove the new number and repeat for the next number
 
         ans = []
         dfs([], 1)
