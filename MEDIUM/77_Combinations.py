7ab2ecdbff15dbd6a2001466635481eeaa31f97e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-04-23 15:39:52 -0400

    added solution for 77

--- MEDIUM/77_Combinations.py
+++ MEDIUM/77_Combinations.py
@@ -4,7 +4,22 @@ Given two integers n and k, return all possible combinations of k numbers chosen
 You may return the answer in any order.
 """
 
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        
+        def dfs(curr, first):
+            if len(curr) == k:
+                ans.append(curr[:])
+                return
 
+            for num in range(first, n + 1):
+                curr.append(num)
+                dfs(curr, num + 1)
+                curr.pop()
+
+        ans = []
+        dfs([], 1)
+        return ans
 
 """
 
