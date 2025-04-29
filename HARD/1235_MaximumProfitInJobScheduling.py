67950c571f79ac514a985496ac4a1cb22efeca02
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-04-29 15:58:57 -0400

    added code for 1235

--- HARD/1235_MaximumProfitInJobScheduling.py
+++ HARD/1235_MaximumProfitInJobScheduling.py
@@ -7,7 +7,32 @@ If you choose a job that ends at time X you will be able to start another job th
 
 """
 
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+        # create tuples containing start, end, and profit
+        intervals = sorted(zip(startTime, endTime, profit)) # sorted based on start time
+        # create cache for caching
+        cache = {}
 
+        def dfs(i):
+            if i == len(intervals):
+                return 0
+            if i in cache:
+                return cache[i]
+            
+            # dont include interval i
+            res = dfs(i + 1)
+
+            # include interval i
+            j = i + 1
+            # find j where start time > current intervals start time
+            # use binary search (bisect) to avoid time limit exceeded
+            j = bisect.bisect(intervals, (intervals[i][1], -1, -1))
+            
+            cache[i] = res = max(res, intervals[i][2] + dfs(j))
+            return res
+
+        return dfs(0)
 
 """
 
