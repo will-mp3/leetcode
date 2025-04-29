c8d4516c9de357600ebae488608c34926020abf9
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-04-29 17:23:21 -0400

    added code for 767

--- MEDIUM/767_ReorganizeString.py
+++ MEDIUM/767_ReorganizeString.py
@@ -4,7 +4,33 @@ Given a string s, rearrange the characters of s so that any two adjacent charact
 Return any possible rearrangement of s or return "" if not possible.
 """
 
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        count = Counter(s) # hashmap, counts each character and returns hashmap with counts
+        # create max heap using minheap and negatives (python has no maxheap)
+        maxHeap = [[-cnt, char] for char, cnt, in count.items()] # heapify based on first key, -cnt
+        heapq.heapify(maxHeap) # turn our list into a heap
 
+        prev = None # previous used character
+        res = ""
+        while maxHeap or prev:
+            # if previous non null and maxheap is empty, no solution
+            if prev and not maxHeap:
+                return ""
+
+            # most frequent, except prev
+            cnt, char = heapq.heappop(maxHeap)
+            res += char
+            cnt += 1
+
+            if prev:
+                heapq.heappush(maxHeap, prev)
+                prev = None
+
+            if cnt != 0:
+                prev = [cnt, char]
+
+        return res
 
 """
 
