0d17a91e5cd944a5d2dc10c738eef1bc10cf9621
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-04-29 16:05:36 -0400

    added explanation for 1235

--- HARD/1235_MaximumProfitInJobScheduling.py
+++ HARD/1235_MaximumProfitInJobScheduling.py
@@ -35,5 +35,14 @@ class Solution:
         return dfs(0)
 
 """
-
+this problem uses a recursive depth first search algorithm to find its solution.
+the problem can be broken down as a decision tree with two options, use the current interval or dont.
+we take our three arays and zip them together to create these intervals, which contain start, end, and profit.
+we sort these intervals by start time so that we can go through them in an orderly fashion.
+the basic algorithm has us deciding to exclude our current interval i, if so we call our dfs function on the next interval, i + 1.
+if we chose to use our current interval, we have to find the next interval, j, which dosent overlap.
+to find j we use bisect to perform a binary search (on a sorted list) to find the next value greater than our current intervals end time.
+we set our res equal to the max of its current value (the value from not including i above) and our current profit plus dfs(j).
+you'll notice we also make use of caching to avoid repeated work, this is just a hash table that acts as a visited set.
+this solution runs in roughly O(nlogn) thanks to the binary search used in bisect.
 """
