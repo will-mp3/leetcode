6a7c44752a9a384401dc6edfdcc5df19325f751f
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-04 15:51:03 -0400

    added code for 140

--- HARD/140_WordBreakII.py
+++ HARD/140_WordBreakII.py
@@ -5,7 +5,29 @@ Return all such possible sentences in any order.
 Note that the same word in the dictionary may be reused multiple times in the segmentation.
 """
 
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        # optimization for duplicates
+        wordDict = set(wordDict)
 
+        # backtracking solution
+        def backtrack(i):
+            # reached end of the string
+            if i == len(s):
+                res.append(" ".join(cur))
+                return
+            
+            for j in range(i, len(s)):
+                w = s[i:j+1] # build our word from index i to j
+                if w in wordDict: # if our built word is in our list of words
+                    cur.append(w)
+                    backtrack(j + 1) # runs until we reach end of string, then adds all cur words
+                    cur.pop() # once recursive stack pops clear cur word list and start next sentence
+
+        cur = [] # words we have added so far
+        res = []
+        backtrack(0) # start at index 0
+        return res
 
 """
 
