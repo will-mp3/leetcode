f7983926b079902e26a15ca93685705e179023e7
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-04 14:35:48 -0400

    added explanation for 127

--- HARD/127_WordLadder.py
+++ HARD/127_WordLadder.py
@@ -44,5 +44,20 @@ class Solution:
         return 0 # if we dont find the end word return 0
 
 """
-
+this problem has a few things going on, the backbone is an adjacency list we create using wildcard patterns.
+the idea here is we want to turn this into a graph problem since we are dealing with shortest paths.
+our graph would have words who share a single letter difference connected, to simulate this we create patterns of words with one wildcard.
+for example cog would have patterns [*og, c*g, co*], and dog would have patterns [*og, d*g, do*].
+cog and dog would be neighbors since they possess atleast one similar pattern.
+this adjacency list/graph is created using a dictionary, where patterns have the appropriate words mapped to them.
+following the example above we could expect to see *og : [cog, dog].
+now that we have a graph setup, we can use breadth first search to effeciantly find the shortest path to our end word.
+we setup our bfs like we usually would: a visited set (with begin word added), the q (with begin word added), and our result variable.
+our bfs logic is as follows:
+while our queue isnt empty we go through each layer using a for loop, after each layer we add 1 to our result.
+inside the layer we pop our queue and save the word to a variable.
+if this word is our endWord we return result immediately, otherwise we go through all of the characters in our word to create patterns again.
+for each pattern we check if there are any neighbors (if our current pattern is in our neighbor dict).
+we also check if its been visited, if not we add it to visited and add the neighbor to our queue.
+if result is never returned while our queue is active then return 0.
 """
