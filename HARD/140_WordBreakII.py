865ee1148f35e6d60e1870fdd555a54621f9af97
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-04 16:31:08 -0400

    added explanation for 140

--- HARD/140_WordBreakII.py
+++ HARD/140_WordBreakII.py
@@ -30,5 +30,14 @@ class Solution:
         return res
 
 """
-
+this solution makes use of backtracking to repeatedly search through our input string and build words & sentences.
+we track a few variables in this problem, namely a cur list holding out current words and a result list with our sentences to be returned.
+our backtracking function first checks our basecase, which is when we reach the end of our input string.
+when this happens we join and append our current list of words to our result list.
+the main logic takes us through all the characters past index i, which is a passed in parameter.
+each iteration we build a new word to be checked with our word list.
+if the word is present we add it to our current list and call our backtracking function from the next character after our word (j + 1).
+this continues until our base case is found and our recursive stack pops, at this point we start removing words from our current list.
+this recursion allows us to branch when a word, like cat, is found but still be able to check the next characters for a word, like cats.
+ultimately the recursive stack pops through and we return out list of sentences.
 """
