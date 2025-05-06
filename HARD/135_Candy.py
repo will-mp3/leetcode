34abcb338acd8d1bb51e09137c696b28c5dc3d7c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-05 20:06:35 -0400

    added explanation for 135

--- HARD/135_Candy.py
+++ HARD/135_Candy.py
@@ -14,7 +14,7 @@ class Solution:
 
         # first pass, left to right checking left neighbor
         for i in range(1, len(ratings)): # skip index 0, no left neighbor
-            if ratings[i - 1] < ratings[i]:
+            if ratings[i - 1] < ratings[i]: # if i is greater than left neighbor
                 arr[i] = arr[i - 1] + 1
 
         # first pass, right to left checking right neigbor
@@ -26,5 +26,16 @@ class Solution:
         return res
 
 """
-
+this solution dosent rely on any fancy algorithm, the key is being able to visualize the array and its patterns.
+we know that we will need to analyze both neighbors to accurately decide whether to increase the candy count.
+we will have to check both the amount of candy the neighbors have and their rank.
+if we searched left to right in sorted order, we see this is extremely easy, the candies simply follow ascending order.
+the same goes if we do right to left in reverse sorted order.
+the same cannot be said for the standard case where the ranks are mixed.
+to account for this we will search left to right, and only check the left neighbor.
+this allows us to add candies without having to worry about future larger values.
+if the left neighbor is larger we add 1 to the left neighbors cantdy count and store it at index i.
+we then search right to left, checking the right neighbor this time.
+this time we store the max of the right neighbors candy + 1 and the current count.
+try drawing this out, it will make clearer sense.
 """
