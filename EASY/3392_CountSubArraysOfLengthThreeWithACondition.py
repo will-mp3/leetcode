5d0a7f339eb08294198297c6b4de093a2b8557e4
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-06 21:40:29 -0400

    added code for 3392

--- EASY/3392_CountSubArraysOfLengthThreeWithACondition.py
+++ EASY/3392_CountSubArraysOfLengthThreeWithACondition.py
@@ -3,7 +3,13 @@ Given an integer array nums, return the number of subarrays of length 3
 such that the sum of the first and third numbers equals exactly half of the second number.
 """
 
-
+class Solution:
+    def countSubarrays(self, nums: List[int]) -> int:
+        res = 0
+        for i in range(1, len(nums) - 1):
+            if nums[i] == (nums[i - 1] + nums[i + 1]) * 2:
+                res += 1
+        return res
 
 """
 
