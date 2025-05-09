afc1febce680d2d376e6d64dbe2c959a882ba224
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-08 22:18:55 -0400

    added code for 1007

--- MEDIUM/1007_MinimumDominoRotationsForEqualRow.py
+++ MEDIUM/1007_MinimumDominoRotationsForEqualRow.py
@@ -8,7 +8,30 @@ Return the minimum number of rotations so that all the values in tops are the sa
 If it cannot be done, return -1.
 """
 
+class Solution:
+    def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        # common value amoungst all dominoes must be one of the first two values
+        for target in [tops[0], bottoms[0]]: # go through both target values
+            # counts for missing targets on the top or bottom
+            missingT, missingB = 0, 0
 
+            # this allows us to iterate through both lists at once, we get each value in a pair
+            for i, pair in enumerate(zip(tops, bottoms)):
+                # extract values from the pair
+                top, bottom = pair
+
+                # if we find a set that dosent contain either target break and return -1
+                if not (top == target or bottom == target):
+                    break
+                # if top is not our target increment the missing top value
+                if top != target: missingT += 1
+                # if bottom is not our target increment the missing bottom value
+                if bottom != target: missingB += 1
+                # once we get to the end return the minimum of the missing values
+                if i == len(tops) - 1:
+                    return min(missingT, missingB)
+
+        return -1
 
 """
 
