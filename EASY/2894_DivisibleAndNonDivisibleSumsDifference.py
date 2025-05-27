ff4e75702bb2105efccbf49cf54fb82d90febd67
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-27 12:16:22 -0400

    added explanation for 2894

--- EASY/2894_DivisibleAndNonDivisibleSumsDifference.py
+++ EASY/2894_DivisibleAndNonDivisibleSumsDifference.py
@@ -19,5 +19,10 @@ class Solution:
         return num1 - num2
 
 """
-
+this solution makes use of the modulo operator to separate divisible numbers.
+we have to check through the entire list regardless, so we go through number 1 through n (inclusive).
+our iterator i will be the current value we are checking, each loop we use the modulo operator to check if i is divisible by m.
+this is done with a simple fi else statement since i cannot be both divisible and non-divisible.
+at the end we return the sum of num1 - num2.
+this solution runs in O(n) linear time.
 """
