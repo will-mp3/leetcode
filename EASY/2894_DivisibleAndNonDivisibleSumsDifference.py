66a405c18979967cd43797a12ec03734c0a2a438
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-27 12:13:55 -0400

    added code for 2894

--- EASY/2894_DivisibleAndNonDivisibleSumsDifference.py
+++ EASY/2894_DivisibleAndNonDivisibleSumsDifference.py
@@ -8,7 +8,15 @@ num2: The sum of all integers in the range [1, n] (both inclusive) that are divi
 Return the integer num1 - num2.
 """
 
-
+class Solution:
+    def differenceOfSums(self, n: int, m: int) -> int:
+        num1, num2 = 0, 0
+        for i in range(1, n + 1):
+            if i % m != 0:
+                num1 += i 
+            else: 
+                num2 += i
+        return num1 - num2
 
 """
 
