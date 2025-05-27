5183d8930fd646fe1d77c5023a09adc5d3e7f1c4
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-27 13:05:24 -0400

    added code for 9

--- EASY/9_PalindromeNumber.py
+++ EASY/9_PalindromeNumber.py
@@ -2,7 +2,14 @@
 Given an integer x, return true if x is a palindrome, and false otherwise.
 """
 
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        string  = str(x)
 
+        if string == string[::-1]:
+            return True
+        else:
+            return False
 
 """
 
