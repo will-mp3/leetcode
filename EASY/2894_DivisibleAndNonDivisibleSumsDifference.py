1184005ee3d7701dca4f0c3e29f9e8d731a18493
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-27 12:09:51 -0400

    added file for 2894

---
+++ EASY/2894_DivisibleAndNonDivisibleSumsDifference.py
@@ -0,0 +1,15 @@
+"""
+You are given positive integers n and m.
+
+Define two integers as follows:
+
+num1: The sum of all integers in the range [1, n] (both inclusive) that are not divisible by m.
+num2: The sum of all integers in the range [1, n] (both inclusive) that are divisible by m.
+Return the integer num1 - num2.
+"""
+
+
+
+"""
+
+"""
