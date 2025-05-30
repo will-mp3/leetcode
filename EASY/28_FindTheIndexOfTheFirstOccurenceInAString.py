8aea22bfcb481b2c84b601c77cc7bc3d8a90e9ea
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-30 19:04:10 -0400

    added code for 28

--- EASY/28_FindTheIndexOfTheFirstOccurenceInAString.py
+++ EASY/28_FindTheIndexOfTheFirstOccurenceInAString.py
@@ -3,7 +3,19 @@ Given two strings needle and haystack, return the index of the first occurrence 
 or -1 if needle is not part of haystack.
 """
 
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        strlen = len(needle)
+        l, r = 0, strlen - 1
 
+        while r < len(haystack):
+            word = haystack[l:r+1]
+            if word == needle:
+                return l
+            l += 1
+            r += 1
+
+        return -1
 
 """
 
