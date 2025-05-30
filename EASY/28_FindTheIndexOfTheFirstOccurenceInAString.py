fc92b690d661334de31e4c895d72f9361b1cde66
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-05-30 19:52:56 -0400

    added explanation for 28

--- EASY/28_FindTheIndexOfTheFirstOccurenceInAString.py
+++ EASY/28_FindTheIndexOfTheFirstOccurenceInAString.py
@@ -18,5 +18,12 @@ class Solution:
         return -1
 
 """
-
+this solution uses a sliding window technique to find the given word in the given string of words.
+we do this by defining our window length, which is the length of the word we are searching for.
+once found we can set up our pointers to define our window, starting at 0 and our length - 1.
+while our right pointer is still inbounds we build our word based on the characters in our window.
+if this word is equal to our target word, we return the left pointer as the first index its seen.
+otherwise, we increment both pointers to shift our window by 1.
+this loop continues until a match is found or the right pointer goes out of bounds, returning -1.
+this solution runs in O(n) linear time.
 """
