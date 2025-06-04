2b282d77e6d5a03d71bc11462e4ec4616d9ea2f5
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-04 14:42:55 -0400

    added code for 3403

--- MEDIUM/3403_FindTheLExicographicallyLargestStringFromTheBoxI.py
+++ MEDIUM/3403_FindTheLExicographicallyLargestStringFromTheBoxI.py
@@ -8,7 +8,21 @@ All the split words are put into a box.
 Find the lexicographically largest string from the box after all the rounds are finished.
 """
 
+class Solution:
+    def answerString(self, word: str, numFriends: int) -> str:
+        if numFriends == 1:
+            return word
 
+        n = len(word)
+        curMax = ""
+        
+        # sliding window, repeatedly updating max with a window the min between:
+        # (i + n - numFriends + 1), our max answer length formula
+        # n, the length of word (for when we (i + n - numFriends + 1)is out of bounds)
+        for i in range(n):
+            curMax = max(curMax, word[i : min(i + n - numFriends + 1, n)])
+        
+        return curMax
 
 """
 
