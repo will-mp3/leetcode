5cf908f69ee76976df8525fd593a314af47b6909
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-04 14:42:42 -0400

    added file for 3403

---
+++ MEDIUM/3403_FindTheLExicographicallyLargestStringFromTheBoxI.py
@@ -0,0 +1,15 @@
+"""
+You are given a string word, and an integer numFriends.
+
+Alice is organizing a game for her numFriends friends. There are multiple rounds in the game, where in each round:
+
+word is split into numFriends non-empty strings, such that no previous round has had the exact same split.
+All the split words are put into a box.
+Find the lexicographically largest string from the box after all the rounds are finished.
+"""
+
+
+
+"""
+
+"""
