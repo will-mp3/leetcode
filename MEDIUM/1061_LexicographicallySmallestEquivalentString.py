66356e72c632fc26607d4a2a499276ea8775fa9e
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-05 13:11:22 -0400

    added code for 1061

--- MEDIUM/1061_LexicographicallySmallestEquivalentString.py
+++ MEDIUM/1061_LexicographicallySmallestEquivalentString.py
@@ -15,7 +15,34 @@ and "aab" is the lexicographically smallest equivalent string of baseStr.
 Return the lexicographically smallest equivalent string of baseStr by using the equivalency information from s1 and s2.
 """
 
+from collections import defaultdict
 
+class Solution:
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        # make a dictionary of list type
+        adj = defaultdict(list)
+
+        # create the adjacency list
+        for a, b in zip(s1, s2):
+            adj[a].append(b)
+            adj[b].append(a)
+
+        def dfs(char, visited):
+            visited.add(char)
+            curMin = char
+            for neighbor in adj[char]:
+                if neighbor not in visited:
+                    # find the smallest character in each neighbors tree (including the neighbor) and compare that with the current character
+                    candidate = dfs(neighbor, visited)
+                    curMin = min(curMin, candidate)
+            return curMin
+
+        res = []
+        for char in baseStr:
+            visited = set() # resets with each char, avoids repeated work in the same search
+            res.append(dfs(char, visited))
+        
+        return ''.join(res)
 
 """
 
