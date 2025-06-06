c82e6b3b9e9859c5a6257ebd47773eef5625c1aa
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-06 13:00:18 -0400

    added code for 2434

--- MEDIUM/2434_UsingARobotToPrintTheLexicographicallySmallestString.py
+++ MEDIUM/2434_UsingARobotToPrintTheLexicographicallySmallestString.py
@@ -7,7 +7,30 @@ Remove the last character of a string t and give it to the robot. The robot will
 Return the lexicographically smallest string that can be written on the paper.
 """
 
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        stack, res = [], []
+        # list of characters and their frequencys in string s
+        freq = Counter(s)
 
+        def min_char(freq):
+            for i in range(26):
+                char = chr(ord('a') + i) # find the lowercase equivalent of i (ord(a) = unicode of a)
+                if freq[char] > 0: # return first present character in sorted frequency array
+                    return char
+            return 'a'
+
+        for char in s:
+            stack.append(char)
+            freq[char] -= 1
+            while stack and stack[-1] <= min_char(freq): # while our stack has values and the top value <= min char in s
+                res.append(stack.pop())
+
+        # empty remaining stack
+        while stack:
+            res.append(stack.pop())
+
+        return ''.join(res)
 
 """
 
