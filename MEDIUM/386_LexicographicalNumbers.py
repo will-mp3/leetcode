e0ca3fadfdc90658dc8bb7219e29998c3a6c2361
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-08 12:31:43 -0400

    added code for 386

--- MEDIUM/386_LexicographicalNumbers.py
+++ MEDIUM/386_LexicographicalNumbers.py
@@ -4,7 +4,23 @@ Given an integer n, return all the numbers in the range [1, n] sorted in lexicog
 You must write an algorithm that runs in O(n) time and uses O(1) extra space. 
 """
 
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
 
+        def dfs(num):
+            newNum = num * 10
+
+            for i in range(10):
+                if newNum + i <= n: 
+                    res.append(newNum + i)   
+                    dfs(newNum + i)
+                    
+        for i in range(1, 10 if n >= 10 else n + 1):
+            res.append(i)
+            dfs(i)
+        
+        return res
 
 """
 
