f4636b6a3aa0b6ce044b4635360363f5a66fff00
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-12 08:41:48 -0400

    added code for 3424

--- EASY/3424_MaximumDifferenceBetweenAdjacentElementsinACircularArray.py
+++ EASY/3424_MaximumDifferenceBetweenAdjacentElementsinACircularArray.py
@@ -4,7 +4,12 @@ Given a circular array nums, find the maximum absolute difference between adjace
 Note: In a circular array, the first and last elements are adjacent.
 """
 
-
+class Solution:
+    def maxAdjacentDistance(self, nums: List[int]) -> int:
+        maxDif = abs(nums[0] - nums[-1]) # check circular case
+        for i in range(len(nums) - 1):
+            maxDif = max(maxDif, abs(nums[i] - nums[i + 1]))
+        return maxDif
 
 """
 
