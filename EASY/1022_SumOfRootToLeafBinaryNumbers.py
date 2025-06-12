3ed31bc22de44c23ecb45d4b76830877e8460f15
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-11 20:52:43 -0400

    added code for 1022

--- EASY/1022_SumOfRootToLeafBinaryNumbers.py
+++ EASY/1022_SumOfRootToLeafBinaryNumbers.py
@@ -7,7 +7,38 @@ For all leaves in the tree, consider the numbers represented by the path from th
 The test cases are generated so that the answer fits in a 32-bits integer.
 """
 
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        res = []
+        
+        def dfs(node, num):
+            
+            if not node:
+                return
+            
+            num = num + str(node.val)
+            
+            if not node.left and not node.right:
+                res.append(num)
+            
+            dfs(node.left, num)
+            dfs(node.right, num)
+            
+            return
+        
+        dfs(root, '')
+        print(res)
+        
+        val = 0
+        for num in res:
+            val += int(num, 2)
+        return val
 
 """
 
