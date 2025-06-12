47e17b1ee41a989a53925d0e5cbaedeed9828355
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-12 08:41:32 -0400

    added file for 342

---
+++ EASY/3424_MaximumDifferenceBetweenAdjacentElementsinACircularArray.py
@@ -0,0 +1,11 @@
+"""
+Given a circular array nums, find the maximum absolute difference between adjacent elements.
+
+Note: In a circular array, the first and last elements are adjacent.
+"""
+
+
+
+"""
+
+"""
