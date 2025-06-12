f4ad2d19356a9a6f7d3a76695aa9248a341f52e9
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-12 19:29:39 -0400

    fixed prompt

--- MEDIUM/1258_SynonymousSentences.py
+++ MEDIUM/1258_SynonymousSentences.py
@@ -1,4 +1,5 @@
-"""You are given a list of equivalent string pairs synonyms where synonyms[i] = [si, ti] indicates that si and ti are equivalent strings. 
+"""
+You are given a list of equivalent string pairs synonyms where synonyms[i] = [si, ti] indicates that si and ti are equivalent strings. 
 You are also given a sentence text.
 
 Return all possible synonymous sentences sorted lexicographically.
