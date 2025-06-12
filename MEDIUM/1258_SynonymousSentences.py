fc6509ee259cebc59ea3994124fedac5e93a7f78
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-12 19:29:30 -0400

    added file for 1258

---
+++ MEDIUM/1258_SynonymousSentences.py
@@ -0,0 +1,11 @@
+"""You are given a list of equivalent string pairs synonyms where synonyms[i] = [si, ti] indicates that si and ti are equivalent strings. 
+You are also given a sentence text.
+
+Return all possible synonymous sentences sorted lexicographically.
+"""
+
+
+
+"""
+
+"""
