9a61cbca08a8c9d7175ee692e89967fb1df4d29a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-14 09:41:07 -0400

    added explanation for 2616

--- MEDIUM/2616_MinimizeTheMaximumDifferenceOfPairs.py
+++ MEDIUM/2616_MinimizeTheMaximumDifferenceOfPairs.py
@@ -25,7 +25,7 @@ class Solution:
                 index += 1
             return count
         
-        left, right = 0, nums[-1] - nums[0]
+        left, right = 0, nums[-1] - nums[0] # 0 and the max difference in the array
         while left < right:
             mid = left + (right - left) // 2
 
@@ -38,5 +38,15 @@ class Solution:
         return left
 
 """
-
+this problems solution relies on a modified binary search utilizing threshholds.
+the first thing we must notice is thatwe have to sort our array to get the smallest pairs.
+our differences will automatically be sorted as adjacents and we can move two indexes at a time to avoid repeat indices.
+using our new list of numbers, we want to perform a binary search to find the smallest difference efficiently.
+we are going to use a threshold for this rather than an index.
+intuitively we could check al the pairs for diff = 1, diff = 2, etc and find our answer, albeit inneficient.
+this diff value is our threshold, and we check each time if there is atleast p pairs meeting the threshhold.
+to speed things up we use binary search, with our left and right set to 0 and the maximum difference respectively.
+while left < right we get our middle value and calculate the value of pairs with a threshold of mid using a helper function.
+if we have atleast p pairs, we check the left partition for a smaller threshold, if we have less than p check the right partition.
+we return left when the binary search is complete, which will be set to the smallest threshold we found.
 """
