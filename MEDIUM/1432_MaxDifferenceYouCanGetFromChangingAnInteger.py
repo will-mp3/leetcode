a099563c5318585a9a772c34a34bdbb23ed78e34
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-15 19:43:10 -0400

    added code for 1432

--- MEDIUM/1432_MaxDifferenceYouCanGetFromChangingAnInteger.py
+++ MEDIUM/1432_MaxDifferenceYouCanGetFromChangingAnInteger.py
@@ -11,7 +11,28 @@ Return the max difference between a and b.
 Note that neither a nor b may have any leading zeros, and must not be 0.
 """
 
+class Solution:
+    def maxDiff(self, num: int) -> int:
+        # first, find the max value (a) by replacing first non-nine value with 9.
+        num1 = str(num)
+        for x in num1:
+            if x != "9":
+                rep1 = x
+                break
+            rep1 = "9"
+        num1 = num1.replace(rep1, "9")
 
+        # now find the smallest
+        num2 = str(num)
+        if num2[0] == "1":
+            for x in num2:
+                if x != "1" and x != "0":
+                    num2 = num2.replace(x, "0")
+                    break
+        else:
+            num2 = num2.replace(num2[0], "1")
+
+        return int(num1) - int(num2)
 
 """
 
