f6f1a6e2e8f94efc4ff1d62eb59e2dc2405c325c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-16 16:12:17 -0400

    added code for 2016

--- EASY/2016_MaximumDifferenceBetweenIncreasingElements.py
+++ EASY/2016_MaximumDifferenceBetweenIncreasingElements.py
@@ -5,7 +5,21 @@ Given a 0-indexed integer array nums of size n, find the maximum difference betw
 Return the maximum difference. If no such i and j exists, return -1.
 """
 
+class Solution:
+    def maximumDifference(self, nums: List[int]) -> int:
+        l, r = 0, 1
+        maxDif = float("-inf")
 
+        while r < len(nums):
+            if nums[r - 1] < nums[l]:
+                l = r - 1
+            maxDif = max(maxDif, nums[r] - nums[l])
+            r += 1
+
+        if maxDif <= 0:
+            return -1
+        
+        return maxDif
 
 """
 
