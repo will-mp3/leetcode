f2d158fef2deeee1fde6db733bdd808e855b1496
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-18 17:28:54 -0400

    added file for 2966

---
+++ MEDIUM/2966_DivideArrayIntoArraysWithMaxDifference.py
@@ -0,0 +1,15 @@
+"""
+You are given an integer array nums of size n where n is a multiple of 3 and a positive integer k.
+
+Divide the array nums into n / 3 arrays of size 3 satisfying the following condition:
+
+The difference between any two elements in one array is less than or equal to k.
+Return a 2D array containing the arrays. If it is impossible to satisfy the conditions, return an empty array. 
+And if there are multiple answers, return any of them.
+"""
+
+
+
+"""
+
+"""
