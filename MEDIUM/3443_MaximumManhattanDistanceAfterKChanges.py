5d92f4a43c6b0ec81ed5e21066c457e086533c1f
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-20 17:59:09 -0400

    added code for 3443

--- MEDIUM/3443_MaximumManhattanDistanceAfterKChanges.py
+++ MEDIUM/3443_MaximumManhattanDistanceAfterKChanges.py
@@ -12,7 +12,29 @@ Find the maximum Manhattan distance from the origin that can be achieved at any 
 The Manhattan Distance between two cells (xi, yi) and (xj, yj) is |xi - xj| + |yi - yj|.
 """
 
-
+class Solution:
+    def maxDistance(self, s: str, k: int) -> int:
+        ans = 0
+        north = south = east = west = 0
+        
+        for i in range(len(s)):
+            c = s[i]
+            if c == 'N':
+                north += 1
+            elif c == 'S':
+                south += 1
+            elif c == 'E':
+                east += 1
+            elif c == 'W':
+                west += 1
+            
+            x = abs(north - south)
+            y = abs(east - west)
+            md = x + y
+            dis = md + min(2 * k, i + 1 - md) # manhanttan distance plus available changes
+            ans = max(ans, dis)
+        
+        return ans
 
 """
 
