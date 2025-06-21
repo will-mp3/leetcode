679fe79cb47b224199fa79c242aeeac9ddbb68ee
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-21 17:26:23 -0400

    added code for 3085

--- MEDIUM/3085_MinimumDeletionsToMakeStringKSpecial.py
+++ MEDIUM/3085_MinimumDeletionsToMakeStringKSpecial.py
@@ -8,7 +8,31 @@ Here, freq(x) denotes the frequency of the character x in word, and |y| denotes 
 Return the minimum number of characters you need to delete to make word k-special.
 """
 
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        # get our character counts
+        cnt = defaultdict(int)
+        for c in word:
+            cnt[c] += 1
+        
+        # default result, if we delete every character
+        res = len(word)
 
+        # outer loop to test each character
+        for a in cnt.values():
+            deleted = 0 # track deletions
+            # inner loop to compare each character to our current test character
+            for b in cnt.values():
+                # if count of a is more than count of b, delete all of b
+                if a > b:
+                    deleted += b
+                # if count of b is greater than a + k, delete the difference between b and a + k
+                elif b > a + k:
+                    deleted += b - (a + k)
+            # update res
+            res = min(res, deleted)
+
+        return res
 
 """
 
