85d41f95ebb8dc47deec12732eced0b83be94c15
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-23 17:45:36 -0400

    added first solution

--- HARD/2081_SumOfKMirrorNumbers.py
+++ HARD/2081_SumOfKMirrorNumbers.py
@@ -8,3 +8,40 @@ which does not read the same both forward and backward.
 
 Given the base k and the number n, return the sum of the n smallest k-mirror numbers.
 """
+
+# first solution attempt, TLE 73/97 (easiest to understand)
+class Solution:
+    def kMirror(self, k: int, n: int) -> int:
+        
+        # helper function to convert base-10 number to base-k
+        def convert(num, base):
+            if num == 0:
+                return '0'
+            digits = []
+            while num:
+                digits.append(str(num % base))
+                num //= base
+            return ''.join(reversed(digits))
+
+        # helper function to determine palindrome
+        def isPalindrome(num):
+            strNum = str(num)
+            strRev = strNum[::-1]
+
+            return strNum == strRev
+        
+        # main logic
+        cur, res = 1, 0
+
+        while n:
+            if isPalindrome(cur):
+                if isPalindrome(convert(cur, k)):
+                    res += cur
+                    n -= 1
+            cur += 1
+        
+        return res
+
+"""
+
+"""
