77626fb8b23ede080841bd73dff5b4837eb2c8cb
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-26 17:01:09 -0400

    added explanation for 2311

--- MEDIUM/2311_LongestBinarySubsequenceLessThanOrEqualToK.py
+++ MEDIUM/2311_LongestBinarySubsequenceLessThanOrEqualToK.py
@@ -23,5 +23,14 @@ class Solution:
         return len(res)
 
 """
-
+the key to this solution is how we read in our binary stirng s.
+we know that we can include leading zeros, so the max length of our subsequence is the length of s.
+what that leaves us to do is figure out the minimum amount of characters we have to remove to make s less than or equal to k.
+my approach to this is by going through the string backwards and creating a new string with each character seen.
+we have to be careful with how the binary string is formatted so that it reads correctly, we do this be adding new charcters to the front.
+each character we see is a new largest since we are starting from the back, so we want to make sure to add these to the front to retain value.
+each time we check if this new binary string is greater than k, in this case we dont add anything to our string and move on.
+in the event our new string is less than or equal to k, we add the new character.
+in the end we return the length of our result string.
+this solution runs in O(n) linear time.
 """
