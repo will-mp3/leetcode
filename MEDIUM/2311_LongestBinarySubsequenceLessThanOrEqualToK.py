7d22b94aba8532764d4d91f2f054241c8bf2668c
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-26 16:45:57 -0400

    added code for 2311

--- MEDIUM/2311_LongestBinarySubsequenceLessThanOrEqualToK.py
+++ MEDIUM/2311_LongestBinarySubsequenceLessThanOrEqualToK.py
@@ -10,7 +10,17 @@ A subsequence is a string that can be derived from another string by deleting
 some or no characters without changing the order of the remaining characters.
 """
 
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        res = ""
 
+        for i in range(len(s) - 1, -1, -1):
+            if int(s[i] + res, 2) > k:
+                continue
+            else: 
+                res = s[i] + res
+
+        return len(res)
 
 """
 
