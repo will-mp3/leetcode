265dc5dbbbe5e90a9d93ff261d699bee24d1d0b4
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-27 12:41:15 -0400

    added code for 2014

--- HARD/2014_LongestSubsequenceRepeatedKTimes.py
+++ HARD/2014_LongestSubsequenceRepeatedKTimes.py
@@ -13,7 +13,30 @@ Return the longest subsequence repeated k times in string s. If multiple such su
 return the lexicographically largest one. If there is no such subsequence, return an empty string.
 """
 
+class Solution:
+    def longestSubsequenceRepeatedK(self, s: str, k: int) -> str:
+        def isK(sub: str, t: str, k: int) -> bool:
+            count = i = 0
+            for ch in t:
+                if i < len(sub) and ch == sub[i]:
+                    i += 1
+                    if i == len(sub):
+                        i = 0
+                        count += 1
+                        if count == k:
+                            return True
+            return False
 
+        res = ""
+        q = deque([""])
+        while q:
+            curr = q.popleft()
+            for ch in map(chr, range(ord('a'), ord('z') + 1)):
+                nxt = curr + ch
+                if isK(nxt, s, k):
+                    res = nxt
+                    q.append(nxt)
+        return res
 
 """
 
