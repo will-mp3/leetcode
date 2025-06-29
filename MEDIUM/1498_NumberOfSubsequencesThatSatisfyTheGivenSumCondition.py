12124ee6c34795939b234ab1d71889d247df53d6
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-29 11:09:41 -0400

    added file for 1498

---
+++ MEDIUM/1498_NumberOfSubsequencesThatSatisfyTheGivenSumCondition.py
@@ -0,0 +1,12 @@
+"""
+You are given an array of integers nums and an integer target.
+
+Return the number of non-empty subsequences of nums such that the sum of the minimum and maximum element on it is less or equal to target. 
+Since the answer may be too large, return it modulo 10**9 + 7.
+"""
+
+
+
+"""
+
+"""
