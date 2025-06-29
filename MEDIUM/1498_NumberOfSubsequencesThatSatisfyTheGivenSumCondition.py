e4b86c6cbd45e966bee1d4ae959443098fa5f14b
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-29 11:09:52 -0400

    added code for 1498

--- MEDIUM/1498_NumberOfSubsequencesThatSatisfyTheGivenSumCondition.py
+++ MEDIUM/1498_NumberOfSubsequencesThatSatisfyTheGivenSumCondition.py
@@ -5,7 +5,23 @@ Return the number of non-empty subsequences of nums such that the sum of the min
 Since the answer may be too large, return it modulo 10**9 + 7.
 """
 
+class Solution:
+    def numSubseq(self, nums: List[int], target: int) -> int:
+        mod = 10**9 + 7
+        nums.sort()
+        n = len(nums)
 
+        l, r = 0, n - 1
+        res = 0
+
+        while l <= r:
+            if nums[l] + nums[r] <= target:
+                res = (res + (2 ** (r - l))) % mod
+                l += 1
+            else:
+                r -= 1
+            
+        return res
 
 """
 
