47efc6e8e75ddfe10421201457d3c07fcd07521a
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-06-30 12:49:56 -0400

    added code for 594

--- EASY/594_LongestHarmoniousSubsequence.py
+++ EASY/594_LongestHarmoniousSubsequence.py
@@ -4,7 +4,19 @@ We define a harmonious array as an array where the difference between its maximu
 Given an integer array nums, return the length of its longest harmonious subsequence among all its possible subsequences.
 """
 
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        res = 0
+        # build hashmap
+        numMap = {}
+        for num in nums:
+            numMap[num] = 1 + numMap.get(num, 0)
+        
+        for num in nums:
+            if num + 1 in numMap:
+                res = max(res, numMap[num] + numMap[num + 1])
 
+        return res
 
 """
 
