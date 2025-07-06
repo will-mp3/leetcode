6ed9f36183343fdea2eb6b8abd77da3607fb72bb
Will Katabian <will.katabian@outlook.com>
2025-07-06 18:19:25 -0400

    added file for 1394

---
+++ EASY/1394_FindLuckyIntegerInArray.py
@@ -0,0 +1,11 @@
+"""
+Given an array of integers arr, a lucky integer is an integer that has a frequency in the array equal to its value.
+
+Return the largest lucky integer in the array. If there is no lucky integer return -1.
+"""
+
+
+
+"""
+
+"""
