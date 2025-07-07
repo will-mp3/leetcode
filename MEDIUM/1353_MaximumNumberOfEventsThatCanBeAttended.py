3cf5aa65b627c17e3eda4bec34fd631029d63c97
Will Katabian <will.katabian@outlook.com>
2025-07-07 15:11:48 -0400

    added file for 1353

---
+++ MEDIUM/1353_MaximumNumberOfEventsThatCanBeAttended.py
@@ -0,0 +1,7 @@
+"""
+You are given an array of events where events[i] = [startDayi, endDayi]. Every event i starts at startDayi and ends at endDayi.
+
+You can attend an event i at any day d where startTimei <= d <= endTimei. You can only attend one event at any time d.
+
+Return the maximum number of events you can attend.
+"""
