dd99b141ad6f34c4dc89bf3b420c60c64d7d67bd
Will Katabian <will.katabian@outlook.com>
2025-07-07 15:12:08 -0400

    added code for 1353

--- MEDIUM/1353_MaximumNumberOfEventsThatCanBeAttended.py
+++ MEDIUM/1353_MaximumNumberOfEventsThatCanBeAttended.py
@@ -5,3 +5,37 @@ You can attend an event i at any day d where startTimei <= d <= endTimei. You ca
 
 Return the maximum number of events you can attend.
 """
+
+class Solution:
+    def maxEvents(self, events: List[List[int]]) -> int:
+        # Sort events based on start day
+        events.sort()
+        
+        min_heap = []
+        day = 0
+        index = 0
+        n = len(events)
+        result = 0
+
+        while min_heap or index < n:
+            if not min_heap:
+                day = events[index][0]
+            # add all end times for each day
+            while index < n and events[index][0] <= day:
+                # add end time to heap
+                heapq.heappush(min_heap, events[index][1])
+                index += 1
+            # increment result by 1 for each pop
+            heapq.heappop(min_heap)
+            result += 1
+            day += 1 # move day forward
+            
+            # clear out expired events
+            while min_heap and min_heap[0] < day:
+                heapq.heappop(min_heap)
+        
+        return result
+
+"""
+
+"""
