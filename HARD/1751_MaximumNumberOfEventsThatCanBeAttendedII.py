11cc810eb8dcc96a4b3087994d4f4f506c791cbc
Will Katabian <will.katabian@outlook.com>
2025-07-08 15:05:00 -0400

    added code for 1751

--- HARD/1751_MaximumNumberOfEventsThatCanBeAttendedII.py
+++ HARD/1751_MaximumNumberOfEventsThatCanBeAttendedII.py
@@ -8,7 +8,26 @@ Note that the end day is inclusive: that is, you cannot attend two events where 
 Return the maximum sum of values that you can receive by attending events.
 """
 
+class Solution:
+    def maxValue(self, events: List[List[int]], k: int) -> int:
+        events.sort()
+        n = len(events)
+        starts = [start for start, end, value in events]
+        dp = [[-1] * n for _ in range(k + 1)]
 
+        def dfs(cur_index, count):
+            if count == 0 or cur_index == n:
+                return 0
+            if dp[count][cur_index] != -1:
+                return dp[count][cur_index]
+
+            # Find the nearest available event after attending event 0.
+
+            next_index = bisect_right(starts, events[cur_index][1])
+            dp[count][cur_index] = max(dfs(cur_index + 1, count), events[cur_index][2] + dfs(next_index, count - 1))
+            return dp[count][cur_index]
+        
+        return dfs(0, k)
 
 """
 
