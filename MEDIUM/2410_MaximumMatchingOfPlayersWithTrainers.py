e50e5ab335a65b0710186a01cd639a175a9a4e51
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-07-13 13:42:54 -0400

    added explanation for 2410

--- MEDIUM/2410_MaximumMatchingOfPlayersWithTrainers.py
+++ MEDIUM/2410_MaximumMatchingOfPlayersWithTrainers.py
@@ -25,5 +25,11 @@ class Solution:
         return res
 
 """
-
+this solution uses the two pointer technique to repeatedly check and pair players and trainers.
+we start by first sorting our two arrays so that we can approach the problem greedily.
+while both of our pointers are in bounds we check one condition.
+if the current player element is less than or equal to the current trainer element, we can pair the two and move on.
+we do this by incrementing result and moving each pointer one element forward.
+if the current player is greater than (else) the current trainer, we need to shift the trainer pointer and search for a better trainer.
+this solution runs in O(n) linear time.
 """
