d393e670ccfcf017c81ff2375590f4fb8f357fbb
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-07-13 20:39:51 -0400

    added explanation for 1290

--- EASY/1290_ConvertBinaryNumberInALinkedListToInteger.py
+++ EASY/1290_ConvertBinaryNumberInALinkedListToInteger.py
@@ -12,7 +12,7 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
 class Solution:
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
         binStr = ""
@@ -24,5 +24,9 @@ class Solution:
         return int(binStr, 2)
 
 """
-
+this solution uses a simple while loop.
+we initialize an empty string to hold our binary value, and continuously iterate through nodes in our list.
+with each node we add the string value to our binary string and update head to be head.next.
+once head is null we break and return the binary casted version of our string.
+this solution runs in O(n) linear time.
 """
