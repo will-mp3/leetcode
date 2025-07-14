24fb0e1119af206df2932d29569bebdf11e54129
Will Katabian <105826921+will-mp3@users.noreply.github.com>
2025-07-13 20:37:32 -0400

    added code for 1290

--- EASY/1290_ConvertBinaryNumberInALinkedListToInteger.py
+++ EASY/1290_ConvertBinaryNumberInALinkedListToInteger.py
@@ -1,12 +1,27 @@
 """
-Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. The linked list holds the binary representation of a number.
+Given head which is a reference node to a singly-linked list. The value of each node in the linked list is either 0 or 1. 
+The linked list holds the binary representation of a number.
 
 Return the decimal value of the number in the linked list.
 
 The most significant bit is at the head of the linked list.
 """
 
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+        
+class Solution:
+    def getDecimalValue(self, head: Optional[ListNode]) -> int:
+        binStr = ""
 
+        while head:
+            binStr = binStr + str(head.val)
+            head = head.next
+        
+        return int(binStr, 2)
 
 """
 
