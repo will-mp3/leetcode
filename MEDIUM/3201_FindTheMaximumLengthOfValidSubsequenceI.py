fa4241e7720829e672240541e2ce45b1f0f72368
Will Katabian <will.katabian@outlook.com>
2025-07-16 18:10:10 -0400

    added code for 3201

--- MEDIUM/3201_FindTheMaximumLengthOfValidSubsequenceI.py
+++ MEDIUM/3201_FindTheMaximumLengthOfValidSubsequenceI.py
@@ -8,7 +8,16 @@ Return the length of the longest valid subsequence of nums.
 A subsequence is an array that can be derived from another array by deleting some or no elements without changing the order of the remaining elements.
 """
 
-
+class Solution:
+    def maximumLength(self, nums: List[int]) -> int:
+        res = 0
+        for pattern in [[0, 0], [0, 1], [1, 0], [1, 1]]:
+            cnt = 0
+            for num in nums:
+                if num % 2 == pattern[cnt % 2]:
+                    cnt += 1
+            res = max(res, cnt)
+        return res
 
 """
 
