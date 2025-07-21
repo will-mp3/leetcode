ff2bc394ffada529ad91a096fdb380fbdf7dd22b
Will Katabian <will.katabian@outlook.com>
2025-07-20 23:54:41 -0400

    added code for 1957

--- EASY/1957_DeleteCharactersToMakeAFancyString.py
+++ EASY/1957_DeleteCharactersToMakeAFancyString.py
@@ -6,7 +6,25 @@ Given a string s, delete the minimum possible number of characters from s to mak
 Return the final string after the deletion. It can be shown that the answer will always be unique.
 """
 
+class Solution:
+    def makeFancyString(self, s: str) -> str:
+        newStr = ""
 
+        count = 0
+        prev = None
+        for char in s:
+            if char == prev:
+                count += 1
+            else:
+                count = 1
+
+            if count > 2:
+                continue
+            
+            newStr = newStr + char
+            prev = char
+
+        return newStr
 
 """
 
