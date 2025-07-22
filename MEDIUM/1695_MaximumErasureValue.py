0cb181f6c299eeb03e916b9977823cae420499d5
Will Katabian <will.katabian@outlook.com>
2025-07-22 13:48:20 -0400

    edde code for 1695

--- MEDIUM/1695_MaximumErasureValue.py
+++ MEDIUM/1695_MaximumErasureValue.py
@@ -6,7 +6,21 @@ Return the maximum score you can get by erasing exactly one subarray.
 An array b is called to be a subarray of a if it forms a contiguous subsequence of a, that is, if it is equal to a[l],a[l+1],...,a[r] for some (l,r).
 """
 
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        seen = set()
+        l, cur, res = 0, 0, 0
 
+        for r in range(len(nums)):
+            while nums[r] in seen:
+                cur -= nums[l]
+                seen.remove(nums[l])
+                l += 1
+            cur += nums[r]
+            seen.add(nums[r])
+            res = max(res, cur)
+
+        return res
 
 """
 
