2f1e699db8b0d1ca05ae375a8948cec5283c747d
Will Katabian <will.katabian@outlook.com>
2025-07-23 09:08:52 -0400

    added file for 1717

---
+++ MEDIUM/1717_maximumScoreFromRemovingSubstrings.py
@@ -0,0 +1,15 @@
+"""
+You are given a string s and two integers x and y. You can perform two types of operations any number of times.
+
+Remove substring "ab" and gain x points.
+For example, when removing "ab" from "cabxbae" it becomes "cxbae".
+Remove substring "ba" and gain y points.
+For example, when removing "ba" from "cabxbae" it becomes "cabxe".
+Return the maximum points you can gain after applying the above operations on s.
+"""
+
+
+
+"""
+
+"""
