5c9bcec4f0c7a14b23769698b74e4fbe6b78dd71
Will Katabian <will.katabian@outlook.com>
2025-07-25 13:56:28 -0400

    added file for 3487

---
+++ EASY/3487_maximumUniqueSubarraySumAfterDeletion.py
@@ -0,0 +1,15 @@
+"""
+You are given an integer array nums.
+
+You are allowed to delete any number of elements from nums without making it empty. After performing the deletions, select a subarray of nums such that:
+
+All elements in the subarray are unique.
+The sum of the elements in the subarray is maximized.
+Return the maximum sum of such a subarray.
+"""
+
+
+
+"""
+
+"""
