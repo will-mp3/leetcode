40e9e25fe17e1cb169ffd04ad4bff86b7377653d
Will Katabian <will.katabian@outlook.com>
2025-07-26 14:44:56 -0400

    added file for 3480

---
+++ HARD/3480_MaximizeSubarraysAfterRemovingOneConflictingPair.py
@@ -0,0 +1,14 @@
+"""You are given an integer n which represents an array nums containing the numbers from 1 to n in order. 
+Additionally, you are given a 2D array conflictingPairs, where conflictingPairs[i] = [a, b] indicates that a and b form a conflicting pair.
+
+Remove exactly one element from conflictingPairs. 
+Afterward, count the number of non-empty subarrays of nums which do not contain both a and b for any remaining conflicting pair [a, b].
+
+Return the maximum number of subarrays possible after removing exactly one conflicting pair.
+"""
+
+
+
+"""
+
+"""
