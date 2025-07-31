9cc157ab80050ad05145ee9d6a12300ae354c21e
Will Katabian <will.katabian@outlook.com>
2025-07-31 12:03:21 -0400

    added code for 898

--- MEDIUM/898_BitwiseORsOfSubarrays.py
+++ MEDIUM/898_BitwiseORsOfSubarrays.py
@@ -6,7 +6,14 @@ The bitwise OR of a subarray is the bitwise OR of each integer in the subarray. 
 A subarray is a contiguous non-empty sequence of elements within an array.
 """
 
-
+class Solution:
+    def subarrayBitwiseORs(self, arr: List[int]) -> int:
+        ans = set()
+        cur = {0}
+        for x in arr:
+            cur = {x | y for y in cur} | {x}
+            ans |= cur
+        return len(ans)
 
 """
 
