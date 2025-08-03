55308b82b310cb0785c6ec219e5978330cda7500
Will Katabian <will.katabian@outlook.com>
2025-08-03 17:53:16 -0400

    added code for 2106

--- HARD/2106_MaximumFruitsHarvestedAfterAtMostKSteps.py
+++ HARD/2106_MaximumFruitsHarvestedAfterAtMostKSteps.py
@@ -10,7 +10,19 @@ For every position you reach, you harvest all the fruits at that position, and t
 Return the maximum total number of fruits you can harvest.
 """
 
-
+class Solution:
+    def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
+        left = total = res = 0
+        for right in range(len(fruits)):
+            total += fruits[right][1]
+            while left <= right and min(
+                abs(startPos - fruits[left][0]) + fruits[right][0] - fruits[left][0],
+                abs(startPos - fruits[right][0]) + fruits[right][0] - fruits[left][0]
+            ) > k:
+                total -= fruits[left][1]
+                left += 1
+            res = max(res, total)
+        return res
 
 """
 
