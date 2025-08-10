c7596620069d6937888e9951abfdea8dbe1ef9a5
Will Katabian <will.katabian@outlook.com>
2025-08-10 12:14:57 -0400

    added explanation for 55

--- EASY/55_LengthOfLastWord.py
+++ EASY/55_LengthOfLastWord.py
@@ -17,5 +17,8 @@ class Solution:
         return len(res)
 
 """
-
+This solution iterates through the string from the end to the beginning, collecting characters of the last word until it encounters a space after having collected some characters. 
+The length of the last word is then returned.
+The time complexity is O(n), where n is the length of the string, as we may need to traverse the entire string in the worst case. 
+The space complexity is O(1) if we consider the output length as part of the return value, otherwise it is O(m) where m is the length of the last word.
 """
