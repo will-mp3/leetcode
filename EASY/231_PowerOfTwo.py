10f385c95d228f4a4c9f22714d99f9a61dffa4a0
Will Katabian <will.katabian@outlook.com>
2025-08-10 12:15:48 -0400

    added code for 231

--- EASY/231_PowerOfTwo.py
+++ EASY/231_PowerOfTwo.py
@@ -4,7 +4,9 @@ Given an integer n, return true if it is a power of two. Otherwise, return false
 An integer n is a power of two, if there exists an integer x such that n == 2x.
 """
 
-
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n > 0 and str(bin(n)).count('1') == 1
 
 """
 
