5695a332b25254de9d2859ecd92fbce1de15bcc0
Will Katabian <will.katabian@outlook.com>
2025-08-10 15:52:42 -0400

    added code for 869

--- MEDIUM/869_ReorderPowerOf2.py
+++ MEDIUM/869_ReorderPowerOf2.py
@@ -4,7 +4,24 @@ You are given an integer n. We reorder the digits in any order (including the or
 Return true if and only if we can do this so that the resulting number is a power of two.
 """
 
+from itertools import permutations
 
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        string = str(n)
+        arr = list(string)
+        leng = len(string)
+
+        for combo in permutations(arr, leng):
+            num = int("".join(combo))
+
+            if len(str(num)) != leng:
+                continue
+
+            if str(bin(num)).count('1') == 1:
+                return True
+
+        return False
 
 """
 
