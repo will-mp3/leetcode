1a813801d0ee14ec64d1746ab81b366797a99463
Will Katabian <will.katabian@outlook.com>
2025-08-10 21:51:41 -0400

    added code for 2438

--- MEDIUM/2438_RangeProductQueriesOfPowers.py
+++ MEDIUM/2438_RangeProductQueriesOfPowers.py
@@ -9,7 +9,28 @@ Return an array answers, equal in length to queries, where answers[i] is the ans
 Since the answer to the ith query may be too large, each answers[i] should be returned modulo 109 + 7.
 """
 
+class Solution:
+    def productQueries(self, n: int, queries: List[List[int]]) -> List[int]:
+        MOD = 10**9 + 7
 
+        strBin = reversed(str(bin(n)))
+
+        val = 1
+        powers = []
+        for char in strBin:
+            if char == "1":
+                powers.append(int(val))
+            val *= 2
+        print(powers)
+
+        res = []
+        for query in queries:
+            cur = 1
+            for i in range(query[0], query[1] + 1):
+                cur *= powers[i]
+            res.append(cur%MOD)
+        
+        return res
 
 """
 
