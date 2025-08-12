336c743f057d20197bc64a593f8f1b4db29a7b91
Will Katabian <will.katabian@outlook.com>
2025-08-12 16:45:20 -0400

    added file for 2787

---
+++ MEDIUM/2787_WaysToExpressAnIntegerAsSumOfPowers.py
@@ -0,0 +1,15 @@
+"""
+Given two positive integers n and x.
+
+Return the number of ways n can be expressed as the sum of the xth power of unique positive integers, in other words, the number of sets of unique integers [n1, n2, ..., nk] where n = n1x + n2x + ... + nkx.
+
+Since the result can be very large, return it modulo 109 + 7.
+
+For example, if n = 160 and x = 3, one way to express n is n = 23 + 33 + 53.
+"""
+
+
+
+"""
+
+"""
