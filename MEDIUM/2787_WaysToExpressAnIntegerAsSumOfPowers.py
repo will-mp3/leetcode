1dcca9c1e266a936b19619783907a6644ba70530
Will Katabian <will.katabian@outlook.com>
2025-08-12 16:45:32 -0400

    added code for 2787

--- MEDIUM/2787_WaysToExpressAnIntegerAsSumOfPowers.py
+++ MEDIUM/2787_WaysToExpressAnIntegerAsSumOfPowers.py
@@ -8,7 +8,24 @@ Since the result can be very large, return it modulo 109 + 7.
 For example, if n = 160 and x = 3, one way to express n is n = 23 + 33 + 53.
 """
 
+class Solution:
+    def numberOfWays(self, n: int, x: int) -> int:
+        MOD = 10 ** 9 + 7
 
+        powers = []
+        nxt, base = 0, 1
+        while nxt <= n:
+            nxt = base ** x
+            base += 1
+            powers.append(nxt)
+    
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        for p in powers:
+            for s in range(n, p - 1, -1):
+                dp[s] = (dp[s] + dp[s - p]) % MOD
+        
+        return dp[n]
 
 """
 
