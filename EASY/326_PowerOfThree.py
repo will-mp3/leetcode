c4875fea1b74eee93effb51c35c518693d33c08b
Will Katabian <will.katabian@outlook.com>
2025-08-13 15:17:24 -0400

    added code for 326

--- EASY/326_PowerOfThree.py
+++ EASY/326_PowerOfThree.py
@@ -4,7 +4,15 @@ Given an integer n, return true if it is a power of three. Otherwise, return fal
 An integer n is a power of three, if there exists an integer x such that n == 3x.
 """
 
-
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        cur, power = 1, 1
+        while cur <= n:
+            if cur == n:
+                return True
+            cur = 3 ** power
+            power += 1
+        return False
 
 """
 
