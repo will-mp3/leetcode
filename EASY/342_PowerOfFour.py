935858031e06089975c811c06484a442c85bcb68
Will Katabian <will.katabian@outlook.com>
2025-08-15 16:25:12 -0400

    added code for 342

--- EASY/342_PowerOfFour.py
+++ EASY/342_PowerOfFour.py
@@ -4,7 +4,15 @@ Given an integer n, return true if it is a power of four. Otherwise, return fals
 An integer n is a power of four, if there exists an integer x such that n == 4x.
 """
 
-
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        cur, power = 1, 1
+        while cur <= n:
+            if cur == n:
+                return True
+            cur = 4 ** power
+            power += 1
+        return False
 
 """
 
