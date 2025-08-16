9ee5b8f5ee71026245264fc32883fda87efebdb6
Will Katabian <will.katabian@outlook.com>
2025-08-16 14:47:55 -0400

    added code for 1323

--- EASY/1323_Maximum69Number.py
+++ EASY/1323_Maximum69Number.py
@@ -4,7 +4,14 @@ You are given a positive integer num consisting only of digits 6 and 9.
 Return the maximum number you can get by changing at most one digit (6 becomes 9, and 9 becomes 6).
 """
 
-
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        string = str(num)
+        for i in range(len(string)):
+            if string[i] == "6":
+                string = string[:i] + "9" + string[i + 1:]
+                break
+        return int(string)
 
 """
 
