409d9ca4a4f026d356b566b3ca18588726f12428
Will Katabian <will.katabian@outlook.com>
2025-08-19 19:38:04 -0400

    added code for 2348

--- MEDIUM/2348_NumberOfZeroFilledSubarrays.py
+++ MEDIUM/2348_NumberOfZeroFilledSubarrays.py
@@ -4,7 +4,18 @@ Given an integer array nums, return the number of subarrays filled with 0.
 A subarray is a contiguous non-empty sequence of elements within an array.
 """
 
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        res, zeros = 0, 0
 
+        for num in nums:
+            if num == 0:
+                res += zeros + 1
+                zeros += 1
+            else:
+                zeros = 0
+        
+        return res
 
 """
 
