543bdc0d5f491275cd88b383be481abdc4eedf2a
Will Katabian <will.katabian@outlook.com>
2025-08-21 15:20:01 -0400

    added code for 1504

--- MEDIUM/1504_CountSubmatricesWithAllOnes.py
+++ MEDIUM/1504_CountSubmatricesWithAllOnes.py
@@ -2,7 +2,26 @@
 Given an m x n binary matrix mat, return the number of submatrices that have all ones.
 """
 
-
+class Solution:
+    def numSubmat(self, mat: List[List[int]]) -> int:
+        r, c = len(mat), len(mat[0])
+        h = [0] * c
+        ans = 0
+        for i in range(r):
+            for j in range(c):
+                h[j] = h[j] + 1 if mat[i][j] else 0
+            sumv, st = [0] * c, []
+            for j in range(c):
+                while st and h[st[-1]] >= h[j]:
+                    st.pop()
+                if st:
+                    p = st[-1]
+                    sumv[j] = sumv[p] + h[j] * (j - p)
+                else:
+                    sumv[j] = h[j] * (j + 1)
+                st.append(j)
+                ans += sumv[j]
+        return ans
 
 """
 
