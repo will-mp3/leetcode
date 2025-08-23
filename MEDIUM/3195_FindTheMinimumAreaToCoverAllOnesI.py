d6b831a45325c1611d574393769f27be89377453
Will Katabian <will.katabian@outlook.com>
2025-08-23 18:04:44 -0400

    added code for 3195

--- MEDIUM/3195_FindTheMinimumAreaToCoverAllOnesI.py
+++ MEDIUM/3195_FindTheMinimumAreaToCoverAllOnesI.py
@@ -4,7 +4,21 @@ You are given a 2D binary array grid. Find a rectangle with horizontal and verti
 Return the minimum possible area of the rectangle.
 """
 
+class Solution:
+    def minimumArea(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        minRow, maxRow = m, -1
+        minCol, maxCol = n, -1
 
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    minRow = min(minRow, i)
+                    maxRow = max(maxRow, i)
+                    minCol = min(minCol, j)
+                    maxCol = max(maxCol, j)
+
+        return (maxRow - minRow + 1) * (maxCol - minCol + 1)
 
 """
 
