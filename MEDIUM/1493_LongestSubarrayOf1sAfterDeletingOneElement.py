7bd056ff8f47a9d298391344f1e2bebbd1e223db
Will Katabian <will.katabian@outlook.com>
2025-08-24 19:50:19 -0400

    added code for 1493

--- MEDIUM/1493_LongestSubarrayOf1sAfterDeletingOneElement.py
+++ MEDIUM/1493_LongestSubarrayOf1sAfterDeletingOneElement.py
@@ -4,7 +4,22 @@ Given a binary array nums, you should delete one element from it.
 Return the size of the longest non-empty subarray containing only 1's in the resulting array. Return 0 if there is no such subarray.
 """
 
+class Solution:
+    def longestSubarray(self, nums: List[int]) -> int:
+        res = 0
+        counts = defaultdict(int)
 
+        left = 0
+        for i in range(len(nums)):
+            counts[nums[i]] += 1
+
+            while counts[0] > 1:
+                counts[nums[left]] -= 1
+                left += 1
+            
+            res = max(res, counts[1] + counts[0] - 1)
+        
+        return res
 
 """
 
