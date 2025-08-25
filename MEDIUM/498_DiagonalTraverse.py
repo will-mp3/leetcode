e1d7b1bfa775f5b3eae321f7df15163a3d202099
Will Katabian <will.katabian@outlook.com>
2025-08-25 10:52:16 -0400

    added code for 498

--- MEDIUM/498_DiagonalTraverse.py
+++ MEDIUM/498_DiagonalTraverse.py
@@ -2,7 +2,19 @@
 Given an m x n matrix mat, return an array of all the elements of the array in a diagonal order.
 """
 
+class Solution:
+    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
+        groups = defaultdict(list)
+        
+        for i in range(len(mat)):
+            for j in range(len(mat[0])):
+                groups[i + j].append(mat[i][j])
 
+        res = []
+        for i in range(len(mat) + len(mat[0])):
+            res += groups[i] if i % 2 == 1 else groups[i][::-1]
+        
+        return res
 
 """
 
