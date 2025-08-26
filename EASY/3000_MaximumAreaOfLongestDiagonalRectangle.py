81d6c2468c5b3d629d5366633405aaeeba576f4b
Will Katabian <will.katabian@outlook.com>
2025-08-26 09:25:46 -0400

    added code for 3000

--- EASY/3000_MaximumAreaOfLongestDiagonalRectangle.py
+++ EASY/3000_MaximumAreaOfLongestDiagonalRectangle.py
@@ -6,7 +6,20 @@ For all indices i, 0 <= i < dimensions.length, dimensions[i][0] represents the l
 Return the area of the rectangle having the longest diagonal. If there are multiple rectangles with the longest diagonal, return the area of the rectangle having the maximum area.
 """
 
-
+class Solution:
+    def areaOfMaxDiagonal(self, dimensions: List[List[int]]) -> int:
+        res = 0
+        maxDiag = 0
+        
+        for rec in dimensions:
+            diag = math.sqrt((rec[0] * rec[0]) + (rec[1] * rec[1]))
+            if diag > maxDiag:
+                maxDiag = diag
+                res = rec[0] * rec[1]
+            elif diag == maxDiag:
+                res = max(res, rec[0] * rec[1])
+        
+        return res
 
 """
 
