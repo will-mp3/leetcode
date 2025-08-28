46c4acaa6693f990702f9ae3d2d143106c24ecb0
Will Katabian <will.katabian@outlook.com>
2025-08-28 12:46:11 -0400

    added code for 3446

--- MEDIUM/3446_SortMatrixByDiagonals.py
+++ MEDIUM/3446_SortMatrixByDiagonals.py
@@ -5,7 +5,20 @@ The diagonals in the bottom-left triangle (including the middle diagonal) are so
 The diagonals in the top-right triangle are sorted in non-decreasing order.
 """
 
-
+class Solution:
+    def sortMatrix(self, grid: List[List[int]]) -> List[List[int]]:
+        diags = defaultdict(list)
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                diags[i - j].append(grid[i][j])
+                diags[i - j].sort(reverse = False if i - j < 0 else True)
+        
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                grid[i][j] = diags[i - j][min(i, j)]
+        
+        return grid
 
 """
 
