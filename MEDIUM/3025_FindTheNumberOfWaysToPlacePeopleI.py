9ef1529f13f8992249939e77f589e40a4e640cfd
Will Katabian <will.katabian@outlook.com>
2025-09-02 18:15:13 -0400

    added code for 3025

--- MEDIUM/3025_FindTheNumberOfWaysToPlacePeopleI.py
+++ MEDIUM/3025_FindTheNumberOfWaysToPlacePeopleI.py
@@ -8,7 +8,21 @@ there are no other points in the rectangle (or line) they make (including the bo
 Return the count.
 """
 
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        # sort by x descending, then y ascending
+        points.sort(key=lambda p: (-p[0], p[1]))
+        n = len(points)
+        res = 0
 
+        for i in range(n - 1):
+            minY = 1 << 31  # like +infinity
+            for j in range(i + 1, n):
+                # condition: points[i] is upper-left of points[j]
+                if minY > points[j][1] >= points[i][1]:
+                    res += 1
+                    minY = points[j][1]  # update "lowest seen" y
+        return res
 
 """
 
