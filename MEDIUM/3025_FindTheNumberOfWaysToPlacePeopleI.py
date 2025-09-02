ea20365ab4046df8c775cb849c6769238281c9dd
Will Katabian <will.katabian@outlook.com>
2025-09-02 18:15:03 -0400

    added file for 3025

---
+++ MEDIUM/3025_FindTheNumberOfWaysToPlacePeopleI.py
@@ -0,0 +1,15 @@
+"""
+You are given a 2D array points of size n x 2 representing integer coordinates of some points on a 2D plane, where points[i] = [xi, yi].
+
+Count the number of pairs of points (A, B), where
+
+A is on the upper left side of B, and
+there are no other points in the rectangle (or line) they make (including the border).
+Return the count.
+"""
+
+
+
+"""
+
+"""
