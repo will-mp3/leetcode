38a7a7d8945a481d20915d4004e92a105492a7eb
Will Katabian <will.katabian@outlook.com>
2025-09-07 14:51:04 -0400

    added code for 1304

--- EASY/1304_FindNUniqueIntegersSumUpToZero.py
+++ EASY/1304_FindNUniqueIntegersSumUpToZero.py
@@ -2,7 +2,13 @@
 Given an integer n, return any array containing n unique integers such that they add up to 0.
 """
 
-
+class Solution:
+    def sumZero(self, n: int) -> List[int]:
+        res = [0] if n % 2 == 1 else []
+        for i in range(1, n // 2 + 1):
+            res.append(i)
+            res.append(-i)
+        return res
 
 """
 
