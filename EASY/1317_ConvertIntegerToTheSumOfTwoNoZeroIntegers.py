d163167e966cc6da3cbc023c9c5235b43acd397d
Will Katabian <will.katabian@outlook.com>
2025-09-08 19:10:59 -0400

    added code for 1317

--- EASY/1317_ConvertIntegerToTheSumOfTwoNoZeroIntegers.py
+++ EASY/1317_ConvertIntegerToTheSumOfTwoNoZeroIntegers.py
@@ -8,7 +8,18 @@ a + b = n
 The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
 """
 
-
+class Solution:
+    def getNoZeroIntegers(self, n: int) -> List[int]:
+        a = n // 2
+        b = n - a
+        zeros = str(a).count("0") + str(b).count("0")
+        
+        while zeros > 0:
+            a += 1
+            b -= 1
+            zeros = str(a).count("0") + str(b).count("0")
+        
+        return [a, b]
 
 """
 
