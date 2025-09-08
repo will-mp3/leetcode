ee98dcaf0c3ce16f859b8124fd2428259c5beba1
Will Katabian <will.katabian@outlook.com>
2025-09-08 19:10:49 -0400

    added file for 1317

---
+++ EASY/1317_ConvertIntegerToTheSumOfTwoNoZeroIntegers.py
@@ -0,0 +1,15 @@
+"""
+No-Zero integer is a positive integer that does not contain any 0 in its decimal representation.
+
+Given an integer n, return a list of two integers [a, b] where:
+
+a and b are No-Zero integers.
+a + b = n
+The test cases are generated so that there is at least one valid solution. If there are many valid solutions, you can return any of them.
+"""
+
+
+
+"""
+
+"""
