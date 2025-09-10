f5dc1c0e19367d6d0887f8982513e7bdb5c83ef6
Will Katabian <will.katabian@outlook.com>
2025-09-10 19:58:05 -0400

    added code for 1733

--- MEDIUM/1733_MinimumNumberOfPeopleToTeach.py
+++ MEDIUM/1733_MinimumNumberOfPeopleToTeach.py
@@ -11,7 +11,36 @@ You can choose one language and teach it to some users so that all friends can c
 Note that friendships are not transitive, meaning if x is a friend of y and y is a friend of z, this doesn't guarantee that x is a friend of z.
 """
 
+class Solution:
+    def minimumTeachings(self, n: int, languages: List[List[int]], friendships: List[List[int]]) -> int:
+        users_to_teach = set()
 
+        # Step 1: Identify users who can't communicate
+        for user1, user2 in friendships:
+            user1 -= 1  # Convert to 0-based index
+            user2 -= 1
+            can_communicate = False
+
+            for lang1 in languages[user1]:
+                if lang1 in languages[user2]:
+                    can_communicate = True
+                    break
+
+            if not can_communicate:
+                users_to_teach.add(user1)
+                users_to_teach.add(user2)
+
+        # Step 2: Try teaching each language
+        min_users_to_teach = len(languages) + 1
+
+        for language in range(1, n + 1):
+            count = 0
+            for user in users_to_teach:
+                if language not in languages[user]:
+                    count += 1
+            min_users_to_teach = min(min_users_to_teach, count)
+
+        return min_users_to_teach
 
 """
 
