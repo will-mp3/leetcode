42fde8a3afa8d825bbc6d35c0ccdc30901180d28
Will Katabian <will.katabian@outlook.com>
2025-09-15 17:21:07 -0400

    added code for 1935

--- EASY/1935_MaximumNumberOfWordsYouCanType.py
+++ EASY/1935_MaximumNumberOfWordsYouCanType.py
@@ -4,7 +4,19 @@ There is a malfunctioning keyboard where some letter keys do not work. All other
 Given a string text of words separated by a single space (no leading or trailing spaces) and a string brokenLetters of all distinct letter keys that are broken, return the number of words in text you can fully type using this keyboard.
 """
 
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        words = text.split(" ")
+        letters = list(brokenLetters)
 
+        res = len(words)
+        for word in words:
+            for letter in letters:
+                if letter in word:
+                    res -= 1
+                    break
+        
+        return res
 
 """
 
