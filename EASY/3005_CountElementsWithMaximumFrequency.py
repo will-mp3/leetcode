5e613e49617ba41c8f84c732588891f7204529fa
Will Katabian <will.katabian@outlook.com>
2025-09-22 18:50:00 -0400

    added code for 3005

--- EASY/3005_CountElementsWithMaximumFrequency.py
+++ EASY/3005_CountElementsWithMaximumFrequency.py
@@ -6,7 +6,21 @@ Return the total frequencies of elements in nums such that those elements all ha
 The frequency of an element is the number of occurrences of that element in the array.
 """
 
+class Solution:
+    def maxFrequencyElements(self, nums: List[int]) -> int:
+        counts = defaultdict(int)
+        maxC = 0
 
+        for num in nums:
+            counts[num] += 1
+            maxC = max(maxC, counts[num])
+        
+        res = 0
+        for count in counts.values():
+            if count == maxC:
+                res += count
+
+        return res
 
 """
 
