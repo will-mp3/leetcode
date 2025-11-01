e3da70598fa672ba6691c556dc4135f22434436f
Will Katabian <will.katabian@outlook.com>
2025-11-01 15:04:52 -0400

    Added code for 3217.

--- MEDIUM/3217_DeleteNodesFromLinkedListPresentInArray.py
+++ MEDIUM/3217_DeleteNodesFromLinkedListPresentInArray.py
@@ -2,7 +2,22 @@
 You are given an array of integers nums and the head of a linked list. Return the head of the modified linked list after removing all nodes from the linked list that have a value that exists in nums.
 """
 
-
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def modifiedList(self, nums: List[int], head: Optional[ListNode]) -> Optional[ListNode]:
+        numSet = set(nums)
+        dummy = ListNode(None)
+        cur = dummy
+        while head:
+            if head.val not in numSet:
+                cur.next = ListNode(head.val)
+                cur = cur.next
+            head = head.next
+        return dummy.next
 
 """
 
