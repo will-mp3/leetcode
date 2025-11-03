9f6e07b438dc71cdb40d72ffd867c27967a0f367
Will Katabian <will.katabian@outlook.com>
2025-11-03 17:01:18 -0500

    Added code for 1578.

--- MEDIUM/1578_MinimumTimeToMakeRopeColorful.py
+++ MEDIUM/1578_MinimumTimeToMakeRopeColorful.py
@@ -6,7 +6,22 @@ Alice wants the rope to be colorful. She does not want two consecutive balloons 
 Return the minimum time Bob needs to make the rope colorful.
 """
 
-
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        n, sum_ = len(colors), 0
+        i = 1
+        while i < n:
+            maxi = 0
+            while i < n and colors[i] == colors[i - 1]:
+                sum_ += neededTime[i - 1]
+                maxi = max(maxi, neededTime[i - 1])
+                i += 1
+            sum_ += neededTime[i - 1]
+            maxi = max(maxi, neededTime[i - 1])
+            if maxi != 0:
+                sum_ -= maxi
+            i += 1
+        return sum_
 
 """
 
