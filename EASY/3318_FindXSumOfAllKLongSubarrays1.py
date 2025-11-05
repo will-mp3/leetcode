074db3c0b3f9cc659cbf9060d31eb90f449781d9
Will Katabian <will.katabian@outlook.com>
2025-11-05 18:44:05 -0500

    Added code for 3318.

--- EASY/3318_FindXSumOfAllKLongSubarrays1.py
+++ EASY/3318_FindXSumOfAllKLongSubarrays1.py
@@ -11,7 +11,16 @@ Note that if an array has less than x distinct elements, its x-sum is the sum of
 Return an integer array answer of length n - k + 1 where answer[i] is the x-sum of the subarray nums[i..i + k - 1].
 """
 
-
+class Solution:
+    def findXSum(self, nums: List[int], k: int, x: int) -> List[int]:
+        n = len(nums)
+        ans = list()
+        for i in range(n - k + 1):
+            cnt = Counter(nums[i : i + k])
+            freq = sorted(cnt.items(), key=lambda item: (-item[1], -item[0]))
+            xsum = sum(key * value for key, value in freq[:x])
+            ans.append(xsum)
+        return ans
 
 """
 
