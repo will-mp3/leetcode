9c48ee31ebd5f2e6dabdc85a9be61047caacf288
Will Katabian <will.katabian@outlook.com>
2025-11-10 17:57:13 -0500

    Added code for 3542.

--- MEDIUM/3542_MinimumOperationsToConvertAllElementsToZero.py
+++ MEDIUM/3542_MinimumOperationsToConvertAllElementsToZero.py
@@ -6,7 +6,19 @@ In one operation, you can select a subarray [i, j] (where 0 <= i <= j < n) and s
 Return the minimum number of operations required to make all elements in the array 0.
 """
 
-
+class Solution:
+    def minOperations(self, nums: list[int]) -> int:
+        stack = []
+        res = 0
+        for n in nums:
+            while stack and stack[-1] > n:
+                stack.pop()
+            if n == 0:
+                continue
+            if not stack or stack[-1] < n:
+                res += 1
+                stack.append(n)
+        return res
 
 """
 
