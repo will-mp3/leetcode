ea03e9312b7f2d5329832ffc05e64bc3a5654dab
Will Katabian <will.katabian@outlook.com>
2025-11-11 17:40:16 -0500

    Added code for 474.

--- MEDIUM/474_OnesAndZZeroes.py
+++ MEDIUM/474_OnesAndZZeroes.py
@@ -6,7 +6,31 @@ Return the size of the largest subset of strs such that there are at most m 0's 
 A set x is a subset of a set y if all elements of x are also elements of y.
 """
 
+class Solution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = {(0, 0): 0}
 
+        for s in strs:
+            ones = 0
+            zeroes = 0
+            for ch in s:
+                if ch == "0":
+                    zeroes += 1
+                else:
+                    ones += 1
+            newdp = {}
+
+            for k, v in dp.items():
+                prevzeroes, prevones = k
+                newzeroes, newones = prevzeroes + zeroes, prevones + ones
+                if newzeroes <= m and newones <= n:
+                    if (newzeroes, newones) not in dp:
+                        newdp[(newzeroes, newones)] = v + 1
+
+                    elif dp[(newzeroes, newones)] < v + 1:
+                        newdp[(newzeroes, newones)] = v + 1
+            dp.update(newdp)
+        return max(dp.values())
 
 """
 
