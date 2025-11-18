c8d9eb2ff36a0fcb4239a5925148d42a1f796dc4
Will Katabian <will.katabian@outlook.com>
2025-11-18 16:55:48 -0500

    file for 717

---
+++ EASY/717_1bitAnd2BitCharacters.py
@@ -0,0 +1,13 @@
+"""
+We have two special characters:
+
+The first character can be represented by one bit 0.
+The second character can be represented by two bits (10 or 11).
+Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
+"""
+
+
+
+"""
+
+"""
