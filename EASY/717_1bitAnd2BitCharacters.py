199ef68d61aacaac7ab065396cf79541175f3b7d
Will Katabian <will.katabian@outlook.com>
2025-11-18 16:55:59 -0500

    code for 717

--- EASY/717_1bitAnd2BitCharacters.py
+++ EASY/717_1bitAnd2BitCharacters.py
@@ -6,7 +6,15 @@ The second character can be represented by two bits (10 or 11).
 Given a binary array bits that ends with 0, return true if the last character must be a one-bit character.
 """
 
-
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        n = len(bits)
+        i = 0
+        while i < n - 1:
+          # see 0, move one space; see 1, move two space
+            i += bits[i] + 1
+        # check if we landed at the last bit or skipped it
+        return i == n - 1
 
 """
 
