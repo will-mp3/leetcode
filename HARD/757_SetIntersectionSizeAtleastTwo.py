623caf5cb83c557867371cd319bf4f05c228b282
Will Katabian <will.katabian@outlook.com>
2025-11-20 18:02:43 -0500

    file for 757

---
+++ HARD/757_SetIntersectionSizeAtleastTwo.py
@@ -0,0 +1,14 @@
+"""
+You are given a 2D integer array intervals where intervals[i] = [starti, endi] represents all the integers from starti to endi inclusively.
+
+A containing set is an array nums where each interval from intervals has at least two integers in nums.
+
+For example, if intervals = [[1,3], [3,7], [8,9]], then [1,2,4,7,8,9] and [2,3,4,8,9] are containing sets.
+Return the minimum possible size of a containing set.
+"""
+
+
+
+"""
+
+"""
