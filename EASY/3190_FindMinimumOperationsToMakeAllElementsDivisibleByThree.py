fb8930d3c3b3b50c958b81517c19dd8081a197b2
Will Katabian <will.katabian@outlook.com>
2025-11-22 16:24:53 -0500

    code for 3190

--- EASY/3190_FindMinimumOperationsToMakeAllElementsDivisibleByThree.py
+++ EASY/3190_FindMinimumOperationsToMakeAllElementsDivisibleByThree.py
@@ -4,7 +4,13 @@ You are given an integer array nums. In one operation, you can add or subtract 1
 Return the minimum number of operations to make all elements of nums divisible by 3.
 """
 
-
+class Solution:
+    def minimumOperations(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+          if num % 3:
+            res += min(num % 3, 3 - (num % 3))
+        return res
 
 """
 
