d5624aa2bb4deef97ca953d4cab6e4f78fc2bd2c
Will Katabian <will.katabian@outlook.com>
2025-11-24 11:55:35 -0500

    code for 1018

--- EASY/1018_BinaryPreficDivisibleBy5.py
+++ EASY/1018_BinaryPreficDivisibleBy5.py
@@ -7,7 +7,13 @@ For example, if nums = [1,0,1], then x0 = 1, x1 = 2, and x2 = 5.
 Return an array of booleans answer where answer[i] is true if xi is divisible by 5.
 """
 
-
+class Solution:
+    def prefixesDivBy5(self, nums: List[int]) -> List[bool]:
+        val = 0
+        for i in range(len(nums)):
+            val = ((val << 1) + nums[i]) % 5
+            nums[i] = val == 0
+        return nums
 
 """
 
