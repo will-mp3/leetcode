e605f61dc4bf34d41a2bc03486cf85f0275b34f8
Will Katabian <will.katabian@outlook.com>
2025-11-27 11:22:46 -0500

    code for 3381.

--- MEDIUM/3381_MaximumSubarraySumWithLengthDivisibleByK.py
+++ MEDIUM/3381_MaximumSubarraySumWithLengthDivisibleByK.py
@@ -4,7 +4,18 @@ You are given an array of integers nums and an integer k.
 Return the maximum sum of a subarray of nums, such that the size of the subarray is divisible by k.
 """
 
-
+class Solution:
+    def maxSubarraySum(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        prefixSum = 0
+        maxSum = -sys.maxsize
+        kSum = [sys.maxsize // 2] * k
+        kSum[k - 1] = 0
+        for i in range(n):
+            prefixSum += nums[i]
+            maxSum = max(maxSum, prefixSum - kSum[i % k])
+            kSum[i % k] = min(kSum[i % k], prefixSum)
+        return maxSum
 
 """
 
