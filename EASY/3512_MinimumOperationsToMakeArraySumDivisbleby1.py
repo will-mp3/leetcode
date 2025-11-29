e47f2127ca0397d850116b1491b481be349b0c8a
Will Katabian <will.katabian@outlook.com>
2025-11-28 20:43:46 -0500

    file for 3512

---
+++ EASY/3512_MinimumOperationsToMakeArraySumDivisbleby1.py
@@ -0,0 +1,12 @@
+"""
+You are given an integer array nums and an integer k. You can perform the following operation any number of times:
+
+Select an index i and replace nums[i] with nums[i] - 1.
+Return the minimum number of operations required to make the sum of the array divisible by k.
+"""
+
+
+
+"""
+
+"""
