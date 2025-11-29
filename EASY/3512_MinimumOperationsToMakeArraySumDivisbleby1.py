ae474051203b4c11e673a6fb174e5f055e34b40e
Will Katabian <will.katabian@outlook.com>
2025-11-28 20:44:01 -0500

    code for 3512

--- EASY/3512_MinimumOperationsToMakeArraySumDivisbleby1.py
+++ EASY/3512_MinimumOperationsToMakeArraySumDivisbleby1.py
@@ -5,7 +5,9 @@ Select an index i and replace nums[i] with nums[i] - 1.
 Return the minimum number of operations required to make the sum of the array divisible by k.
 """
 
-
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        return sum(nums) % k
 
 """
 
