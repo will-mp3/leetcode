0b4b647563e5a3712bb6c1bff22f9ad69c6096d1
Will Katabian <will.katabian@outlook.com>
2025-12-02 14:54:49 -0500

    file for 3623

---
+++ MEDIUM/3623_CountNumberOfTrapezoidsI.py
@@ -0,0 +1,15 @@
+"""
+You are given a 2D integer array points, where points[i] = [xi, yi] represents the coordinates of the ith point on the Cartesian plane.
+
+A horizontal trapezoid is a convex quadrilateral with at least one pair of horizontal sides (i.e. parallel to the x-axis). Two lines are parallel if and only if they have the same slope.
+
+Return the number of unique horizontal trapezoids that can be formed by choosing any four distinct points from points.
+
+Since the answer may be very large, return it modulo 109 + 7.
+"""
+
+
+
+"""
+
+"""
