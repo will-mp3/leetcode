a57d5d6247e20ecd70e7ff996dfc5ea777f5b02f
Will Katabian <will.katabian@outlook.com>
2025-12-07 20:44:17 -0500

    code for 1523

--- EASY/1523_CountOddNumbersInAnIntervalRange.py
+++ EASY/1523_CountOddNumbersInAnIntervalRange.py
@@ -2,7 +2,9 @@
 Given two non-negative integers low and high. Return the count of odd numbers between low and high (inclusive).
 """
 
-
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        return (high + 1) // 2 - (low // 2)
 
 """
 
