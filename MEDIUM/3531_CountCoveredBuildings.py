4767e43c693a1c8022a9e11ce8b47b6581aa1e12
Will Katabian <will.katabian@outlook.com>
2025-12-11 19:41:44 -0500

    file for 3531

---
+++ MEDIUM/3531_CountCoveredBuildings.py
@@ -0,0 +1,13 @@
+"""
+You are given a positive integer n, representing an n x n city. You are also given a 2D grid buildings, where buildings[i] = [x, y] denotes a unique building located at coordinates [x, y].
+
+A building is covered if there is at least one building in all four directions: left, right, above, and below.
+
+Return the number of covered buildings.
+"""
+
+
+
+"""
+
+"""
