ab6fe591077eb28a583b350e492e9a96269f952b
Will Katabian <will.katabian@outlook.com>
2025-12-11 19:41:55 -0500

    code for 3531

--- MEDIUM/3531_CountCoveredBuildings.py
+++ MEDIUM/3531_CountCoveredBuildings.py
@@ -6,7 +6,28 @@ A building is covered if there is at least one building in all four directions: 
 Return the number of covered buildings.
 """
 
+class Solution:
+    def countCoveredBuildings(self, n: int, buildings: List[List[int]]) -> int:
+        rmax = [0] * (n + 1)
+        rmin = [n + 1] * (n + 1)
+        cmax = [0] * (n + 1)
+        cmin = [n + 1] * (n + 1)
 
+        # Track extreme buildings for each row and column
+        for x, y in buildings:
+            rmax[y] = max(rmax[y], x)
+            rmin[y] = min(rmin[y], x)
+            cmax[x] = max(cmax[x], y)
+            cmin[x] = min(cmin[x], y)
+
+        ans = 0
+
+        # A building is covered only if it's strictly inside both extremes
+        for x, y in buildings:
+            if rmin[y] < x < rmax[y] and cmin[x] < y < cmax[x]:
+                ans += 1
+
+        return ans
 
 """
 
